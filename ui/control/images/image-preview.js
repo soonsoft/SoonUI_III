29a455f0b6b71c45732c017eee0cc607dece36a3
soonsoft <soonz@aliyun.com>
2017-04-11 15:28:40 +0800

    Create image-preview.js

---
+++ ui/control/images/image-preview.js
@@ -0,0 +1,364 @@
+//图片预览视图
+ui.define("ui.ctrls.ImagePreview", {
+    _getOption: function () {
+        return {
+            chooserButtonSize: 16,
+            imageMargin: 10,
+            //vertical | horizontal
+            direction: "horizontal"
+        };
+    },
+    _getEvents: function () {
+        return ["changing", "changed", "ready"];
+    },
+    _create: function () {
+        this.element.addClass("image-preview");
+        this.viewer = this.element.children(".image-view-panel");
+        this.chooser = this.element.children(".image-preview-chooser");
+        
+        if(this.viewer.length == 0) {
+            throw new Error("需要设置一个class为image-view-panel的元素");
+        }
+        if(this.chooser.length == 0) {
+            throw new Error("需要设置一个class为image-preview-chooser的元素");
+        }
+        
+        this.isHorizontal = this.option.direction === "horizontal";
+        if(!ui.core.type(this.option.chooserButtonSize) || this.option.chooserButtonSize < 2) {
+            this.option.chooserButtonSize = 16;
+        }
+        this.item = [];
+
+        this._init();
+    },
+    _init: function () {
+        this.chooserQueue = $("<div class='chooser-queue' />");
+        this.chooserPrev = $("<a href='javascript:void(0)' class='chooser-button font-highlight-hover'></a>");
+        this.chooserNext = $("<a href='javascript:void(0)' class='chooser-button font-highlight-hover'></a>");
+        this.chooser.append(this.chooserPrev)
+            .append(this.chooserQueue)
+            .append(this.chooserNext);
+        
+        this.chooserPrev.click($.proxy(function(e) {
+            this.beforeItems();
+        }, this));
+        this.chooserNext.click($.proxy(function(e) {
+            this.afterItems();
+        }, this));
+        
+        this.chooserAnimator = ui.animator({
+            target: this.chooserQueue,
+            ease: ui.AnimationStyle.easeFromTo
+        });
+        
+        var buttonSize = this.option.chooserButtonSize,
+            showCss = null;
+        if(this.isHorizontal) {
+            this.smallImageSize = this.chooser.height();
+            this.chooserAnimator[0].onChange = function(val) {
+                this.target.scrollLeft(val);
+            };
+            showCss = {
+                "width": buttonSize + "px",
+                "height": "100%"
+            };
+            this.chooserPrev
+                .append("<i class='fa fa-angle-left'></i>")
+                .css(showCss);
+            this.chooserNext
+                .append("<i class='fa fa-angle-right'></i>")
+                .css(showCss)
+                .css("right", "0px");
+            this.isOverflow = function() {
+                return this.chooserQueue[0].scrollWidth > this.chooserQueue.width();
+            };
+            this.showChooserButtons = function() {
+                this.chooserPrev.css("display", "block");
+                this.chooserNext.css("display", "block");
+                this.chooserQueue.css({
+                    "left": buttonSize + "px",
+                    "width": this.chooser.width() - this.option.chooserButtonSize * 2 + "px"
+                });
+            };
+            this.hideChooserButtons = function() {
+                this.chooserPrev.css("display", "none");
+                this.chooserNext.css("display", "none");
+                this.chooserQueue.css({
+                    "left": "0px",
+                    "width": "100%"
+                });
+            };
+        } else {
+            this.smallImageSize = this.chooser.width();
+            this.chooserAnimator[0].onChange = function(val) {
+                this.target.scrollTop(val);
+            };
+            showCss = {
+                "height": buttonSize + "px",
+                "width": "100%",
+                "line-height": buttonSize + "px"
+            };
+            this.chooserPrev
+                .append("<i class='fa fa-angle-up'></i>")
+                .css(showCss);
+            this.chooserNext
+                .append("<i class='fa fa-angle-down'></i>")
+                .css(showCss)
+                .css("bottom", "0px");
+            showCss = {
+                "display": "block"
+            };
+            this.isOverflow = function() {
+                return this.chooserQueue[0].scrollHeight > this.chooserQueue.height();
+            };
+            this.showChooserButtons = function() {
+                this.chooserPrev.css("display", "block");
+                this.chooserNext.css("display", "block");
+                this.chooserQueue.css({
+                    "top": buttonSize + "px",
+                    "height": this.chooser.height() - buttonSize * 2 + "px"
+                });
+            };
+            this.hideChooserButtons = function() {
+                this.chooserPrev.css("display", "none");
+                this.chooserNext.css("display", "none");
+                this.chooserQueue.css({
+                    "top": "0px",
+                    "height": "100%"
+                });
+            };
+        }
+        this.chooserQueue.click($.proxy(this._onClickHandler, this));
+        
+        this.setImages(this.option.images);
+    },
+    _initImages: function(images) {
+        var width, 
+            height,
+            marginValue = 0;
+        var i = 0, 
+            len = images.length,
+            image,
+            item, img,
+            css;
+        height = this.smallImageSize - 4;
+        width = height;
+
+        this.imageSource = images;
+        for(; i < len; i++) {
+            image = images[i];
+            css = this._getImageDisplay(width, height, image.width, image.height);
+            item = $("<div class='small-img' />");
+            item.attr("data-index", i);
+            img = $("<img alt='' />");
+            img.css({
+                width: css.width,
+                height: css.height,
+                "margin-top": css.top,
+                "margin-left": css.left
+            });
+            img.prop("src", image.src);
+            item.append(img);
+            this.chooserQueue.append(item);
+
+            if(this.isHorizontal) {
+                item.css("left", marginValue + "px");
+                marginValue += this.option.imageMargin + item.outerWidth();
+            } else {
+                item.css("top", marginValue + "px");
+                marginValue += this.option.imageMargin + item.outerHeight();
+            }
+            this.items.push(item);
+        }
+        
+        if(this.isOverflow()) {
+            this.showChooserButtons();
+        } else {
+            this.hideChooserButtons();
+        }
+
+        if(this.imageViewer.currentIndex >= 0) {
+            this.selectItem(this.imageViewer.currentIndex);
+            this.fire("changed", this.imageViewer.currentIndex);
+        }
+    },
+    _getImageDisplay: function(displayWidth, displayHeight, imgWidth, imgHeight) {
+        var width,
+            height;
+        var css = {
+            top: "0px",
+            left: "0px"
+        };
+        if (displayWidth > displayHeight) {
+            height = displayHeight;
+            width = Math.floor(imgWidth * (height / imgHeight));
+            if (width > displayWidth) {
+                width = displayWidth;
+                height = Math.floor(imgHeight * (width / imgWidth));
+                css.top = Math.floor((displayHeight - height) / 2) + "px";
+            } else {
+                css.left = Math.floor((displayWidth - width) / 2) + "px";
+            }
+        } else {
+            width = displayWidth;
+            height = Math.floor(imgHeight * (width / imgWidth));
+            if (height > displayHeight) {
+                height = displayHeight;
+                width = Math.floor(imgWidth * (height / imgHeight));
+                css.left = Math.floor((displayWidth - width) / 2) + "px";
+            } else {
+                css.top = Math.floor((displayHeight - height) / 2) + "px";
+            }
+        }
+        css.width = width + "px";
+        css.height = height + "px";
+        return css;
+    },
+    _onClickHandler: function(e) {
+        var elem = $(e.target),
+            nodeName = elem.nodeName();
+        if(elem.hasClass("chooser-queue")) {
+            return;
+        }
+        if(nodeName === "IMG") {
+            elem = elem.parent();
+        }
+        var index = parseInt(elem.attr("data-index"), 10);
+        if(this.fire("changing", index) === false) {
+            return;
+        }
+        if(this.selectItem(index) === false) {
+            return;
+        }
+        this.imageViewer.showImage(index);
+    },
+    selectItem: function(index) {
+        var elem = this.items[index];
+        if(this.currentChooser) {
+            if(this.currentChooser[0] === elem[0]) {
+                return false;
+            }
+            this.currentChooser
+                .removeClass("chooser-selected")
+                .removeClass("border-highlight");
+        }
+        this.currentChooser = elem;
+        this.currentChooser
+            .addClass("chooser-selected")
+            .addClass("border-highlight");
+        if(this.isOverflow()) {
+            this._moveChooserQueue(index);
+        }
+    },
+    empty: function() {
+        this.items = [];
+        this.chooserQueue.empty();
+        
+        if(this.imageViewer) {
+            this.imageViewer.empty();
+        }
+    },
+    setImages: function(images) {
+        if(!Array.isArray(images) || images.length == 0) {
+            return;
+        }
+        this.empty();
+        
+        this.option.images = images;
+        var that = this;
+        if(!this.imageViewer) {
+            this.imageViewer = this.viewer.imageViewer(this.option);
+            this.imageViewer.ready(function(e, images) {
+                that._initImages(images);
+                that.fire("ready");
+            });
+            this.imageViewer.changed(function(e, index) {
+                that.selectItem(index);
+                that.fire("changed", index);
+            });
+        } else {
+            this.imageViewer.setImages(images);
+        }
+    },
+    _caculateScrollValue: function(fn) {
+        var currentValue,
+            caculateValue,
+            queueSize,
+            scrollLength;
+        if(this.isHorizontal) {
+            queueSize = this.chooserQueue.width();
+            currentValue = this.chooserQueue.scrollLeft();
+            scrollLength = this.chooserQueue[0].scrollWidth;
+        } else {
+            queueSize = this.chooserQueue.height();
+            currentValue = this.chooserQueue.scrollTop();
+            scrollLength = this.chooserQueue[0].scrollHeight;
+        }
+        
+        caculateValue = fn.call(this, queueSize, currentValue);
+        if(caculateValue < 0) {
+            caculateValue = 0;
+        } else if(caculateValue > scrollLength - queueSize) {
+            caculateValue = scrollLength - queueSize;
+        }
+        return {
+            from: currentValue,
+            to: caculateValue
+        };
+    },
+    _moveChooserQueue: function(index) {
+        var scrollValue = this._caculateScrollValue(function(queueSize, currentValue) {
+            var fullSize = this.smallImageSize + this.option.imageMargin,
+                count = Math.floor(queueSize / fullSize),
+                beforeCount = Math.floor(count / 2);
+            var scrollCount = index - beforeCount;
+            if(scrollCount < 0) {
+                return 0;
+            } else if(scrollCount + count > this.items.length - 1) {
+                return this.items.length * fullSize;
+            } else {
+                return scrollCount * fullSize;
+            }
+        });
+        this._setScrollValue(scrollValue);
+    },
+    _setScrollValue: function(scrollValue) {
+        if(isNaN(scrollValue.to)) {
+            return;
+        }
+        this.chooserAnimator.stop();
+        var option = this.chooserAnimator[0];
+        if(Math.abs(scrollValue.from - scrollValue.to) < this.smallImageSize) {
+            option.onChange.call(option, scrollValue.to);
+        } else {
+            option.begin = scrollValue.from;
+            option.end = scrollValue.to;
+            this.chooserAnimator.start();
+        }
+    },
+    beforeItems: function() {
+        var scrollValue = this._caculateScrollValue(function(queueSize, currentValue) {
+            var fullSize = this.smallImageSize + this.option.imageMargin,
+                count = Math.floor(queueSize / fullSize),
+                currentCount = Math.floor(currentValue / fullSize);
+            return (currentCount + count * -1) * fullSize;
+        });
+        this._setScrollValue(scrollValue);
+    },
+    afterItems: function() {
+        var scrollValue = this._caculateScrollValue(function(queueSize, currentValue) {
+            var fullSize = this.smallImageSize + this.option.imageMargin,
+                count = Math.floor(queueSize / fullSize),
+                currentCount = Math.floor(currentValue / fullSize);
+            return (currentCount + count) * fullSize;
+        });
+        this._setScrollValue(scrollValue);
+    }
+});
+
+$.fn.imagePreview = function(option) {
+    if(this.length == 0) {
+        return;
+    }
+    return ui.ctrls.ImagePreview(option, this);
+};
