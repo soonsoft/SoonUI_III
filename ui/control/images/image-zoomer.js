f66ae0c37f9462da6277055599a38dc34fa9fa45
soonsoft <soonz@aliyun.com>
2017-04-11 14:34:49 +0800

    Create image-zoomer.js

---
+++ ui/control/images/image-zoomer.js
@@ -0,0 +1,374 @@
+function getLargeImageSrc(img) {
+    var src = img.attr("data-large-src");
+    if(!src) {
+        src = img.prop("src");
+    }
+    return src;
+}
+
+function loadImageSize(src) {
+    var promise = new Promise(function(resolve, reject) {
+        var reimg = new Image();
+        var size = {
+            src: src,
+            width: -1,
+            height: -1
+        };
+        reimg.onload = function () {
+            reimg.onload = null;
+            size.width = reimg.width;
+            size.height = reimg.height;
+            resolve(size);
+        };
+        reimg.onerror = function () {
+            reject(size);
+        };
+        reimg.src = src;
+    });
+    return promise;
+}
+
+//图片放大器
+ui.define("ui.ctrls.ImageZoomer", {
+    _getOption: function () {
+        return {
+            parentContent: $(document.body),
+            onNext: null,
+            onPrev: null,
+            hasNext: null,
+            hasPrev: null,
+            getLargeImageSrc: null
+        };
+    },
+    _getEvents: function () {
+        return ["hided"];
+    },
+    _create: function () {
+        this.parentContent = this.option.parentContent;
+        this.closeButton = null;
+        this.mask = null;
+        this.width;
+        this.height;
+
+        this.target = null;
+        this.targetTop;
+        this.targetLeft;
+
+        if($.isFunction(this.option.getLargeImageSrc)) {
+            this._getLargeImageSrc = this.option.getLargeImageSrc;
+        } else {
+            this._getLargeImageSrc = getLargeImageSrc;
+        }
+
+        this._init();
+    },
+    _init: function () {
+        this.imagePanel = $("<div class='show-image-panel' />");
+        this.currentView = $("<div class='image-view-panel' style='display:none;' />");
+        this.nextView = $("<div class='image-view-panel' style='display:none;' />");
+        this.currentView.append("<img class='image-view-img' />");
+        this.nextView.append("<img class='image-view-img' />");
+        this.closeButton = $("<a class='close-button font-highlight-hover' href='javascript:void(0)'>×</a>");
+        
+        var that = this;
+        this.closeButton.click(function () {
+            that.hide();
+        });
+        
+        this.imagePanel
+            .append(this.currentView)
+            .append(this.nextView)
+            .append(this.closeButton);
+        if($.isFunction(this.option.onNext)) {
+            this.nextButton = $("<a class='next-button font-highlight-hover disabled-button' style='right:10px;' href='javascript:void(0)'><i class='fa fa-angle-right'></i></a>");
+            this.nextButton.click(function(e) {
+                that._doNextView();
+            });
+            this.imagePanel.append(this.nextButton);
+        }
+        if($.isFunction(this.option.onPrev)) {
+            this.prevButton = $("<a class='prev-button font-highlight-hover disabled-button' style='left:10px;' href='javascript:void(0)'><i class='fa fa-angle-left'></i></a>");
+            this.prevButton.click(function(e) {
+                that._doPrevView();
+            });
+            this.imagePanel.append(this.prevButton);
+        }
+        $(document.body).append(this.imagePanel);
+        
+        ui.page.resize(function(e) {
+            that.resizeZoomImage();
+        }, ui.eventPriority.ctrlResize);
+        
+        if(this.prevButton || this.nextButton) {
+            this.changeViewAnimator = ui.animator({
+                ease: ui.AnimationStyle.easeFromTo,
+                onChange: function(val) {
+                    this.target.css("left", val + "px");
+                }
+            }).addTarget({
+                ease: ui.AnimationStyle.easeFromTo,
+                onChange: function(val) {
+                    this.target.css("left", val + "px");
+                }
+            });
+        }
+    },
+    _showOptionButtons: function() {
+        if(this.prevButton) {
+            this.prevButton.removeClass("disabled-button");
+        }
+        if(this.nextButton) {
+            this.nextButton.removeClass("disabled-button");
+        }
+    },
+    _hideOptionButtons: function() {
+        if(this.prevButton) {
+            this.prevButton.addClass("disabled-button");
+        }
+        if(this.nextButton) {
+            this.nextButton.addClass("disabled-button");
+        }
+    },
+    _updateButtonState: function() {
+        if($.isFunction(this.option.hasNext)) {
+            if(this.option.hasNext.call(this)) {
+                this.nextButton.removeClass("disabled-button");
+            } else {
+                this.nextButton.addClass("disabled-button");
+            }
+        }
+        if($.isFunction(this.option.hasPrev)) {
+            if(this.option.hasPrev.call(this)) {
+                this.prevButton.removeClass("disabled-button");
+            } else {
+                this.prevButton.addClass("disabled-button");
+            }
+        }
+    },
+    show: function (target) {
+        this.target = target;
+        var content = this._setImageSize();
+        if (!content) {
+            return;
+        }
+        var img = this.currentView.children("img");
+        img.prop("src", this.target.prop("src"));
+        img.css({
+            "width": this.target.width() + "px",
+            "height": this.target.height() + "px",
+            "left": this.targetLeft + "px",
+            "top": this.targetTop + "px"
+        });
+        this.imagePanel.css({
+            "display": "block",
+            "width": content.parentW + "px",
+            "height": content.parentH + "px",
+            "left": content.parentLoc.left + "px",
+            "top": content.parentLoc.top + "px"
+        });
+        this.currentView.css("display", "block");
+        var left = (content.parentW - this.width) / 2;
+        var top = (content.parentH - this.height) / 2;
+        
+        var that = this;
+        ui.mask.open({
+            opacity: .8
+        });
+        img.animate({
+            "left": left + "px",
+            "top": top + "px",
+            "width": this.width + "px",
+            "height": this.height + "px"
+        }, 240, function() {
+            that._updateButtonState();
+        });
+    },
+    hide: function () {
+        var that = this,
+            img = this.currentView.children("img");
+        ui.mask.close();
+        img.animate({
+            "top": this.targetTop + "px",
+            "left": this.targetLeft + "px",
+            "width": this.target.width() + "px",
+            "height": this.target.height() + "px"
+        }, 240, function() {
+            that._hideOptionButtons();
+            that.imagePanel.css("display", "none");
+            that.currentView.css("display", "none");
+            that.fire("hided", that.target);
+        });
+    },
+    _doNextView: function() {
+        if(this.changeViewAnimator.isStarted) {
+            return;
+        }
+        var nextImg = this.option.onNext.call(this);
+        if(!nextImg) {
+            return;
+        }
+        this._doChangeView(nextImg, function() {
+            this.target = nextImg;
+            this._updateButtonState();
+            this._changeView(-this.parentContent.width());
+        });
+    },
+    _doPrevView: function() {
+        var prevImg;
+        if(this.changeViewAnimator.isStarted) {
+            return;
+        }
+        prevImg = this.option.onPrev.call(this);
+        if(!prevImg) {
+            return;
+        }
+        this._doChangeView(prevImg, function() {
+            this.target = prevImg;
+            this._updateButtonState();
+            this._changeView(this.parentContent.width());
+        });
+    },
+    _doChangeView: function(changeImg, action) {
+        var largeSize = changeImg.data("LargeSize"),
+            that = this;
+        if(largeSize) {
+            action.call(this);
+        } else {
+            loadImageSize(this._getLargeImageSrc(changeImg))
+                .then(
+                    //success
+                    function(size) {
+                        changeImg.data("LargeSize", size);
+                        action.call(that);
+                    },
+                    //failed
+                    function (size) {
+                        action.call(that);
+                    }
+                );
+        }
+    },
+    _changeView: function(changeValue) {
+        var temp = this.currentView;
+        this.currentView = this.nextView;
+        this.nextView = temp;
+        var largeSrc = this._getLargeImageSrc(this.target);
+
+        var content = this._setImageSize();
+        if (!content) {
+            return;
+        }
+        var img = this.currentView.children("img");
+        img.prop("src", largeSrc);
+        img.css({
+            "left": (content.parentW - this.width) / 2 + "px",
+            "top": (content.parentH - this.height) / 2 + "px",
+            "width": this.width + "px",
+            "height": this.height + "px"
+        });
+        this.currentView.css("display", "block");
+        this.currentView.css("left", (-changeValue) + "px");
+        
+        var option = this.changeViewAnimator[0];
+        option.target = this.nextView;
+        option.begin = 0;
+        option.end = changeValue;
+        
+        option = this.changeViewAnimator[1];
+        option.target = this.currentView;
+        option.begin = -changeValue;
+        option.end = 0;
+        
+        var that = this;
+        this.changeViewAnimator.start().done(function() {
+            that.nextView.css("display", "none");
+        });
+        
+    },
+    resizeZoomImage: function () {
+        var content = this._setImageSize();
+        if (!content) {
+            return;
+        }
+        var left = (content.parentW - this.width) / 2;
+        var top = (content.parentH - this.height) / 2;
+        
+        this.imagePanel.css({
+            "width": content.parentW + "px",
+            "height": content.parentH + "px",
+        });
+        var img = this.currentView.children("img");
+        img.css({
+            "left": left + "px",
+            "top": top + "px",
+            "width": this.width + "px",
+            "height": this.height + "px"
+        });
+    },
+    _getActualSize: function (img) {
+        var largeSize = img.data("LargeSize");
+        var mem, w, h;
+        if(!largeSize) {
+            //保存原来的尺寸  
+            mem = { w: img.width(), h: img.height() };
+            //重写
+            img.css({
+                "width": "auto",
+                "height": "auto"
+            });
+            //取得现在的尺寸 
+            w = img.width();
+            h = img.height();
+            //还原
+            img.css({
+                "width": mem.w + "px",
+                "height": mem.h + "px"
+            });
+            largeSize = { width: w, height: h };
+        }
+        
+        return largeSize;
+    },
+    _setImageSize: function () {
+        if (!this.currentView) {
+            return;
+        }
+        if (!this.target) {
+            return;
+        }
+        var img = this.currentView.children("img");
+        img.stop();
+        
+        var size = this._getActualSize(this.target);
+
+        var parentH = this.parentContent.height();
+        var parentW = this.parentContent.width();
+        var imageW = size.width;
+        var imageH = size.height;
+        if (imageW / parentW < imageH / parentH) {
+            if(imageH >= parentH) {
+                this.height = parentH;
+            } else {
+                this.height = imageH;
+            }
+            this.width = Math.floor(imageW * (this.height / imageH));
+        } else {
+            if(imageW >= parentW) {
+                this.width = parentW;
+            } else {
+                this.width = imageH;
+            }
+            this.height = Math.floor(imageH * (this.width / imageW));
+        }
+        var loc = this.target.offset();
+        var parentLoc = this.parentContent.offset();
+        this.targetTop = loc.top - parentLoc.top;
+        this.targetLeft = loc.left - parentLoc.left;
+        var content = {
+            parentW: parentW,
+            parentH: parentH,
+            parentLoc: parentLoc
+        };
+        return content;
+    }
+});
