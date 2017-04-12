b35b05f4f9a70cd85782f1fad6edd331e3fdb61c
soonsoft <soonz@aliyun.com>
2017-04-12 12:34:07 +0800

    Update util-structure-transform.js

--- ui/util-structure-transform.js
+++ ui/util-structure-transform.js
@@ -1,40 +1,41 @@
 // 数据结构转换
 
+var flagFieldKey = "_from-list";
+
+function getFieldMethod(field, fieldName) {
+    if (!$.isFunction(field)) {
+        if (ui.core.isString(field)) {
+            return function () {
+                return this[field];
+            };
+        } else {
+            throw new TypeError(ui.str.textFormat("the {0} is not String or Function.", fieldName));
+        }
+    }
+    return field;
+}
+
 ui.trans = {
     // Array结构转Tree结构
     listToTree: function (list, parentField, valueField, childrenField) {
         if (!$.isArray(list) || list.length === 0)
             return null;
         var tempList = {}, temp, root,
-            item, i, id, pid,
-            flagField = "fromList";
-        var pField, vField;
-        if (!$.isFunction(parentField)) {
-            if (ui.core.type(parentField) === "string") {
-                pField = parentField;
-                parentField = function () {
-                    return this[pField];
-                };
-            } else {
-                throw new TypeError("parentField isn't String or Function");
-            }
-        }
-        if (!$.isFunction(valueField)) {
-            if (ui.core.type(valueField) === "string") {
-                vField = valueField;
-                valueField = function () {
-                    return this[vField];
-                };
-            } else {
-                throw new TypeError("valueField isn't String or Function");
-            }
-        }
-        if (ui.core.type(childrenField) !== "string") {
-            childrenField = "children";
-        }
+            item, i, len, id, pid,
+            flagField = flagFieldKey,
+            key;
 
-        for (i = 0; i < list.length; i++) {
+        parentField = getFieldMethod(parentField, "parentField");
+        valueField = getFieldMethod(valueField, "valueField");
+        childrenField = ui.core.isString(childrenField) 
+                    ? childrenField 
+                    : "children";
+
+        for (i = 0, len = list.length; i < len; i++) {
             item = list[i];
+            if(item === null || item === undefined) {
+                continue;
+            }
             pid = parentField.call(item) + "" || "__";
             if (tempList.hasOwnProperty(pid)) {
                 temp = tempList[pid];
@@ -57,7 +58,7 @@ ui.trans = {
                 tempList[id] = item;
             }
         }
-        for (var key in tempList) {
+        for (key in tempList) {
             if(tempList.hasOwnProperty(key)) {
                 temp = tempList[key];
                 if (!temp.hasOwnProperty(flagField)) {
@@ -69,7 +70,43 @@ ui.trans = {
         return root[childrenField];
     },
     // Array结构转分组结构(两级树结构)
-    listToGroup: function() {
+    listToGroup: function(list, groupField, createGroupItemFn, childrenField) {
+        if (!$.isArray(list) || list.length === 0)
+            return null;
 
+        var temp = {},
+            i, len, key, 
+            groupKey, item, result;
+        
+        groupKey = ui.core.isString(groupField) ? groupField : "text";
+        groupField = getFieldMethod(groupField, "groupField");
+        childrenField = ui.core.isString(childrenField) 
+                    ? childrenField 
+                    : "children";
+        
+        for (i = 0, len = list.length; i < len; i++) {
+            item = list[i];
+            if(item === null || item === undefined) {
+                continue;
+            }
+            key = groupField.call(item) + "" || "__";
+            if(!temp.hasOwnProperty(key)) {
+                temp[key] = {};
+                temp[key][groupKey] = key;
+                temp[key][childrenField] = [];
+                if(ui.core.isFunction(createGroupItemFn)) {
+                    createGroupItemFn.call(this, item, key);
+                }
+            }
+            temp[key][childrenField].push(item);
+        }
+
+        result = [];
+        for(key in temp) {
+            if(temp.hasOwnProperty(key)) {
+                result.push(temp[key]);
+            }
+        }
+        return result;
     }
 };
