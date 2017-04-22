94e0b53b520343a8e0e56325ff0eeac217c1704a
soonsoft <soonz@aliyun.com>
2017-04-22 17:35:12 +0800

    Update grid-view.js

--- ui/control/view/grid-view.js
+++ ui/control/view/grid-view.js
@@ -1,1 +1,411 @@
-// GridView
+// grid view
+
+var cellCheckbox = "grid-checkbox",
+    cellCheckboxAll = "grid-checkbox-all",
+    lastCell = "last-cell",
+    sortClass = "fa-sort",
+    asc = "fa-sort-asc",
+    desc = "fa-sort-desc";
+
+function preparePager(option) {
+    if(option.showPageInfo === true) {
+        if(!option.pageInfoFormatter) {
+            option.pageInfoFormatter = {
+                currentRowNum: function(val) {
+                    return "<span>本页" + val + "行</span>";
+                },
+                rowCount: function(val) {
+                    return "<span class='font-highlight'>共" + val + "行</span>";
+                },
+                pageCount: function(val) {
+                    return "<span>" + val + "页</span>";
+                }
+            };
+        }
+    }
+
+    this.pager = ui.ctrls.Pager(option);
+    this.pageIndex = this.pager.pageIndex;
+    this.pageSize = this.pager.pageSize;
+}
+function reverse(arr1, arr2) {
+    var temp,
+        i = 0, 
+        j = arr1.length - 1,
+        len = arr1.length / 2;
+    for (; i < len; i++, j--) {
+        temp = arr1[i];
+        arr1[i] = arr1[j];
+        arr1[j] = temp;
+
+        temp = arr2[i];
+        arr2[i] = arr2[j];
+        arr2[j] = temp;
+    }
+}
+function defaultSortFn(v1, v2) {
+    var val, i, len;
+    if (Array.isArray(v1)) {
+        val = 0;
+        for (i = 0, len = v1.length; i < len; i++) {
+            val = defaultSorting(v1[i], v2[i]);
+            if (val !== 0) {
+                return val;
+            }
+        }
+        return val;
+    } else {
+        return defaultSorting(v1, v2);
+    }
+}
+function defaultSorting(v1, v2) {
+    if (typeof v1 === "string") {
+        return v1.localeCompare(v2);
+    }
+    if (v1 < v2) {
+        return -1;
+    } else if (v1 > v2) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+// 事件处理函数
+function onSort(e) {
+
+}
+function onTableBodyClick(e) {
+
+}
+
+
+ui.define("ui.ctrls.GridView", {
+    _defineOption: function() {
+        return {
+            /*
+                column property
+                text:       string|function     列名，默认为null
+                column:     string|Array        绑定字段名，默认为null
+                len:        number              列宽度，默认为auto
+                align:      center|left|right   列对齐方式，默认为left(但是表头居中)
+                formatter:  function            格式化器(别名：handler)，默认为null
+                sort:       boolean|function    是否支持排序，true支持，false不支持，默认为false
+            */
+            columns: [],
+            // gridview视图数据
+            viewData: null,
+            // 显示的提示信息
+            promptText: "没有数据",
+            // 高度
+            height: false,
+            // 宽度
+            width: false,
+            // 分页参数
+            pager: {
+                // 当前页码，默认从第1页开始
+                pageIndex: 1,
+                // 记录数，默认100条
+                pageSize: 100,
+                // 显示按钮数量，默认显示10个按钮
+                pageButtonCount: 10,
+                // 是否显示分页统计信息，true|false，默认不显示
+                showPageInfo: false,
+                // 格式化器，包含currentRowNum, rowCount, pageCount的显示
+                pageInfoFormatter: null
+            },
+            // 选择设置
+            selection: {
+                // cell|row|disabled
+                type: "row",
+                // string 排除的标签类型，标记后点击这些标签将不会触发选择事件
+                exclude: false,
+                // 是否可以多选
+                multiple: false
+            }
+        };
+    },
+    _defineEvents: function() {
+        return ["pagechanging", "selecting", "selected", "deselected", "rebind"];
+    },
+    _create: function() {
+        this._selectList = [];
+        this._sorterIndexes = [];
+        this._hasPrompt = !!this.option.promptText;
+        
+        this.gridHead = null;
+        this.gridBody = null;
+        this.columnHeight = 30;
+        this.pagerHeight = 30;
+        
+        if(this.option.pager) {
+            preparePager.call(this, this.option.pager);
+        }
+
+        if(!ui.core.isNumber(this.option.width) || this.option.width <= 0) {
+            this.option.width = false;
+        }
+        if(!ui.core.isNumber(this.option.height) || this.option.height <= 0) {
+            this.option.height = false;
+        }
+
+        // 排序器
+        this.sorter = ui.Introsort();
+        // checkbox勾选计数器
+        this._checkedCount = 0;
+
+        this._init();
+    },
+    _init: function() {
+        this._initBorderWidth();
+        this._initDataPrompt();
+
+        this.gridHead = $("<div class='grid-head' />");
+        this.element.append(this.gridHead);
+        this.createGridHead();
+
+         this.gridBody = $("<div class='grid-body' />");
+        this.element.append(this.gridBody);
+        if (Array.isArray(this.option.viewData)) {
+            this.createGridBody(
+                this.option.viewData, this.option.viewData.length);
+        }
+        this._initPagerPanel();
+
+        this.setSize(this.option.width, this.option.height);
+
+        // event handlers
+        // 排序按钮点击事件
+        this.onSortHandler = $.proxy(onSort, this);
+        // 行或者单元格点击事件
+        this.onTableBodyClickHandler = $.proxy(onTableBodyClick, this)
+    },
+    _initBorderWidth: function() {
+        var getBorderWidth = function(key) {
+            return parseInt(this.element.css(key), 10) || 0;
+        };
+        this.borderWidth = 0;
+        this.borderHeight = 0;
+
+        this.borderWidth += getBorderWidth.call(this, "border-left-width");
+        this.borderWidth += getBorderWidth.call(this, "border-right-width");
+
+        this.borderHeight += getBorderWidth.call(this, "border-top-width");
+        this.borderHeight += getBorderWidth.call(this, "border-bottom-width");
+    },
+    _initDataPrompt: function() {
+        var text;
+        if(this._hasPrompt) {
+            this._dataPrompt = $("<div class='data-prompt' />");
+            text = this.option.promptText;
+            if (ui.core.isString(text) && text.length > 0) {
+                this._dataPrompt.html("<span class='font-highlight'>" + text + "</span>");
+            } else if (ui.core.isFunction(text)) {
+                text = text();
+                this._dataPrompt.append(text);
+            }
+            this.gridBody.append(this._dataPrompt);
+        }
+    },
+    _initPagerPanel: function() {
+        if(this.pager) {
+            this.gridFoot = $("<div class='view-foot' />");
+            this.element.append(this.gridFoot);
+            
+            this.pager.pageNumPanel = $("<div class='page-panel' />");
+            if (this.option.pager.displayDataInfo) {
+                this.pager.pageInfoPanel = $("<div class='data-info' />");
+                this.gridFoot.append(this.pager.pageInfoPanel)
+            } else {
+                this.pager.pageNumPanel.css("width", "100%");
+            }
+
+            this.gridFoot.append(this.pager.pageNumPanel).append($("<br style='clear:both' />"));
+            this.pager.pageChanged(function(pageIndex, pageSize) {
+                this.pageIndex = pageIndex;
+                this.pageSize = pageSize;
+                this.fire("pagechanging", pageIndex, pageSize);
+            }, this);
+        }
+    },
+    // 创建一行的所有单元格
+    _createRowCells: function(tr, rowData, rowIndex) {
+        var i, len, 
+            c, cval, td, el;
+        for (i = 0, len = this.option.columns.length; i < len; i++) {
+            c = this.gridColumns[i];
+            if (!ui.core.isFunction(c.formatter)) {
+                formatter = ui.ColumnStyle.cfn.defaultText;
+            }
+            cval = this._prepareValue(rowData, c);
+            td = this._createCell("td", c);
+            el = formatter.call(this, cval, c, rowIndex, td);
+            if (td.isAnnulment)
+                continue;
+            if (el)
+                td.append(el);
+            if (i === len - 1) {
+                td.addClass(lastCell);
+            }
+            tr.append(td);
+            if(td.isFinale) {
+                td.addClass(lastCell);
+                break;
+            }
+        }
+    },
+    // 获得并组装值
+    _prepareValue: function(rowData, c) {
+
+    },
+    // 获取值
+    _getValue: function() {
+
+    },
+    _createCol: function() {
+
+    },
+    _createCell: function() {
+
+    },
+    _setSorter: function(cell, column, i) {
+
+    },
+    _renderPageList: function(rowCount) {
+        if (!this.pager) {
+            return;
+        }
+        this.pager.data = this.option.viewData;
+        this.pager.pageIndex = this.pageIndex;
+        this.pager.pageSize = this.pageSize;
+        this.pager.renderPageList(rowCount);
+    },
+    _updateScrollState: function() {
+        if (!this.tableHead) return;
+        if(this.gridBody[0].scrollHeight > this.gridBody.height()) {
+            this._headScrollCol.css("width", ui.scrollbarWidth + 0.1 + "px");
+        } else {
+            this._headScrollCol.css("width", "0");
+        }
+    },
+
+
+    /// API
+    /** 创建表头 */
+    createGridHead: function(columns) {
+        var colGroup, thead,
+            tr, th,
+            c, i;
+
+        if(Array.isArray(columns)) {
+            this.option.columns = columns;
+        } else {
+            columns = this.option.columns;
+        }
+
+        if (!this.tableHead) {
+            this.tableHead = $("<table class='table-head' cellspacing='0' cellpadding='0' />");
+            this.gridHead.append(this.tableHead);
+        } else {
+            this.tableHead.html("");
+        }
+
+        colGroup = $("<colgroup />"),
+        thead = $("<thead />");
+        this.tableHead.append(colGroup);
+
+        tr = $("<tr />");
+        for (i = 0; i < columns.length; i++) {
+            c = columns[i];
+            if (!c._columnKeys) {
+                c._columnKeys = {};
+            }
+            colGroup.append(this.createCol(c));
+            th = this._createCell("th", c);
+            if ($.isFunction(c.text)) {
+                th.append(c.text.call(this, c, th));
+            } else {
+                if(c.text) {
+                    th.append(ui.ColumnStyle.cnfn.columnText.call(this, c, th));
+                }
+            }
+            this._setSorter(th, c, i);
+            if (i == columns.length - 1) {
+                th.addClass(lastCell);
+            }
+            tr.append(th);
+        }
+
+        this._headScrollCol = $("<col style='width:0' />");
+        colGroup.append(this._headScrollCol);
+        tr.append($("<th class='scroll-cell' />"));
+
+        thead.append(tr);
+        this.tableHead.append(thead);
+    },
+    /** 创建内容 */
+    createGridBody: function(viewData, rowCount) {
+        var colGroup, tbody,
+            tr, i, j, c,
+            isRebind = false;
+        
+        if (!this.tableBody) {
+            this.tableBody = $("<table class='table-body' cellspacing='0' cellpadding='0' />");
+            if (this.isSelectable()) {
+                this.tableBody.click(this.onTableBodyClickHandler);
+            }
+            this.gridBody.append(this.tableBody);
+        } else {
+            this.gridBody.scrollTop(0);
+            this.clear(false);
+            isRebind = true;
+        }
+
+        if(!Array.isArray(viewData) || viewData.length == 0) {
+            this.showDataPrompt();
+            return;
+        } else {
+            this.hideDataPrompt();
+        }
+        this.option.viewData = viewData;
+
+        colGroup = $("<colgroup />"),
+        tbody = $("<tbody />");
+        this.tableBody.append(colGroup);
+
+        for (j = 0; j < this.option.columns.length; j++) {
+            c = this.option.columns[j];
+            colGroup.append(this._createCol(c));
+        }
+        for (i = 0; i < dataTable.length; i++) {
+            tr = $("<tr />");
+            this._createRowCells(tr, dataTable[i], i);
+            tbody.append(tr);
+        }
+        this.tableBody.append(tbody);
+
+        this._updateScrollState();
+        //update page numbers
+        if (ui.core.isNumber(rowCount)) {
+            this._renderPageList(rowCount);
+        }
+
+        if (isRebind) {
+            this.fire("rebind");
+        }
+    },
+    /** 获取视图数据 */
+    getViewData: function() {
+        return Array.isArray(this.option.viewData) 
+            ? this.option.viewData 
+            : [];
+    },
+    /** 获取项目数 */
+    count: function() {
+        return this.viewData.length;
+    },
+    /** 清空表格数据 */
+    clear: function() {
+
+    }
+});
