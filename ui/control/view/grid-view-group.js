9877ebc769f4d204811fbf4586835699db3e2635
soonsoft <soonz@aliyun.com>
2017-05-02 15:24:54 +0800

    Update grid-view-group.js

--- ui/control/view/grid-view-group.js
+++ ui/control/view/grid-view-group.js
@@ -1,1 +1,140 @@
+// GridView-Group
+function defaultCreateGroupItem(groupKey) {
+    return {
+        groupText: groupKey
+    };
+}
+function isGroupItem() {
+    return ui.core.type(this.itemIndexes) === "array";
+}
+function renderGroupItemCell(data, column, index, td) {
+    td.isFinale = true;
+    td.attr("colspan", this.option.columns.length);
+    td.click(this.groupGrid.onGroupRowClickHandler);
+    this.groupGrid["_last_group_index_"] = data.groupIndex;
+    return this.groupGrid.groupItemFormatter.apply(this, arguments);
+}
 
+function onGropRowClick(e) {
+    var td,
+        groupItem;
+
+    e.stopPropagation();
+    td = $(e.target);
+    while(!td.isNodeName("td")) {
+        if(td.isNodeName("tr")) {
+            return;
+        }
+        td = td.parent();
+    }
+    groupItem = this.gridview.dataTable[td.parent().get(0).rowIndex];
+    if(td.hasClass("group-fold")) {
+        td.removeClass("group-fold");
+        this._operateChildren(groupItem.itemIndexes, function(item, row) {
+            $(row).css("display", "table-row");
+        });
+    } else {
+        td.addClass("group-fold");
+        this._operateChildren(groupItem.itemIndexes, function(item, row) {
+            $(row).css("display", "none");
+        });
+    }
+}
+
+function GroupGrid() {
+    if(this instanceof GroupGrid) {
+        this.initialize();
+    } else {
+        return new GroupGrid();
+    }
+}
+GroupGrid.prototype = {
+    constructor: GroupGrid,
+    initialize: function() {
+        this.gridview = null;
+        this.onGroupRowClickHandler = $.proxy(onGropRowClick, this);
+    },
+    _operateChildren: function (list, action) {
+        var i = 0,
+            len = list.length,
+            rowIndex;
+        var dataTable = this.gridview.dataTable,
+            rows = this.gridview.tableBody[0].rows,
+            item,
+            result;
+        for (; i < len; i++) {
+            rowIndex = list[i];
+            item = dataTable[rowIndex];
+            action.call(this, item, rows[rowIndex]);
+        }
+    },
+
+    /// API
+    /** 绑定GridView */
+    setGridView: function(gridview) {
+        if(gridview instanceof ui.ctrls.GridView) {
+            this.gridview = gridview;
+            this.gridview.groupGrid = this;
+        }
+    },
+    /** 数据结构转换 */
+    listGroup: function(list, groupField, createGroupItem) {
+        var groupList = [];
+        var dict = {};
+        
+        if(!Array.isArray(list) || list.length == 0) {
+            return groupList;
+        }
+        createGroupItem = $.isFunction(createGroupItem) ? createGroupItem : defaultCreateGroupItem;
+        var i = 0,
+            len = list.length;
+        var item,
+            groupKey,
+            groupItem;
+        for(; i < len; i++) {
+            item = list[i];
+            groupKey = item[groupField];
+            if(!dict.hasOwnProperty(groupKey)) {
+                groupItem = createGroupItem.call(item, groupKey);
+                groupItem.itemIndexes = [i];
+                dict[groupKey] = groupItem;
+            } else {
+                dict[groupKey].itemIndexes.push(i);
+            }
+        }
+        for(groupKey in dict) {
+            groupItem = dict[groupKey];
+            groupList.push(groupItem);
+            groupItem.groupIndex = groupList.length - 1;
+            for(i = 0, len = groupItem.itemIndexes.length; i < len; i++) {
+                groupList.push(list[groupItem.itemIndexes[i]]);
+                groupItem.itemIndexes[i] = groupList.length - 1;
+            }
+        }
+        return groupList;
+    },
+    /** 分组行号格式化器，每个分组都会自动调用分组格式化器，并从1开始 */
+    rowNumber: function(value, column, index, td) {
+        var data = this.dataTable[index];
+        if(isGroupItem.call(data)) {
+            return renderGroupItemCell.call(this, data, column, index, td);
+        } else {
+            return "<span>" + (index - this.groupGrid["_last_group_index_"]) + "</span>";
+        }
+    },
+    /** 分组文本格式化器，每次开始新分组都会自动调用分组格式化器 */
+    formatText: function(value, column, index, td) {
+        var data = this.dataTable[index];
+        if(isGroupItem.call(data)) {
+            return renderGroupItemCell.call(this, data, column, index, td);
+        } else {
+            return ui.ColumnStyle.cfn.defaultText.apply(this, arguments);
+        }
+    },
+    /** 分组单元格格式化器，外部需要重写 */
+    groupItemFormatter: function(val, col, idx, td) {
+        return null;
+    }
+};
+
+ui.ctrls.GroupGrid = GroupGrid;
