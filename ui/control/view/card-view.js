838a44748cff3c441acb776e1cbf5f7f6ad87f14
soonsoft <soonz@aliyun.com>
2017-05-05 16:32:56 +0800

    Update card-view.js

--- ui/control/view/card-view.js
+++ ui/control/view/card-view.js
@@ -1,1 +1,536 @@
-//CardView
+// CardView
+
+var selectedClass = "ui-card-view-selection";
+    frameBorderWidth = 4;
+
+function preparePager(option) {
+    if(option.showPageInfo === true) {
+        if(!option.pageInfoFormatter) {
+            option.pageInfoFormatter = {
+                currentRowNum: function(val) {
+                    return "<span>本页" + val + "项</span>";
+                },
+                rowCount: function(val) {
+                    return "<span class='font-highlight'>共" + val + "项</span>";
+                },
+                pageCount: function(val) {
+                    return "<span>" + val + "页</span>";
+                }
+            };
+        }
+    }
+
+    this.pager = ui.ctrls.Pager(option);
+    this.pageIndex = this.pager.pageIndex;
+    this.pageSize = this.pager.pageSize;
+}
+
+// 事件处理函数
+// 选择事件
+function onBodyClick(e) {
+    var elem = $(e.target);
+    while(!elem.hasClass("ui-card-view-item")) {
+        if(elem.hasClass("ui-card-view-panel")) {
+            return;
+        }
+        elem = elem.parent();
+    }
+    this._selectItem(elem);
+}
+
+ui.define("ui.ctrls.CardView", {
+    _defineOption: function() {
+        return {
+            // 视图数据
+            viewData: null,
+            // 没有数据时显示的提示信息
+            promptText: "没有数据",
+            // 高度
+            width: false,
+            // 宽度
+            height: false,
+            // 卡片项宽度
+            itemWidth: 200,
+            // 卡片项高度
+            itemHeight: 200,
+            // 卡片格式化器
+            renderItemFormatter: null,
+            // 分页信息
+            pager: {
+                // 当前页码，默认从第1页开始
+                pageIndex: 1,
+                // 记录数，默认30条
+                pageSize: 30,
+                // 显示按钮数量，默认显示10个按钮
+                pageButtonCount: 10,
+                // 是否显示分页统计信息，true|false，默认不显示
+                showPageInfo: false,
+                // 格式化器，包含currentRowNum, rowCount, pageCount的显示
+                pageInfoFormatter: null
+            },
+            // 选择逻辑单选或多选
+            selection: {
+                multiple: false
+            }
+        };
+    },
+    _defineEvents: function() {
+        var events = ["selecting", "selected", "deselected", "rebind", "cencel"];
+        if(this.option.pager) {
+            events.push("pagechanging");
+        }
+        return events;
+    },
+    _create: function() {
+        this._selectList = [];
+        this._hasPrompt = !!this.option.promptText;
+        this._columnCount = 0;
+
+        this.viewBody = null;
+        this.pagerHeight = 30;
+
+        if(this.option.pager) {
+            preparePager.call(this, this.option.pager);
+        }
+
+        if(!ui.core.isNumber(this.option.width) || this.option.width <= 0) {
+            this.option.width = false;
+        }
+        if(!ui.core.isNumber(this.option.height) || this.option.height <= 0) {
+            this.option.height = false;
+        }
+
+        this._init();
+    },
+    _init: function() {
+        if(!this.element || this.element.length === 0) {
+            return;
+        }
+
+        this._initBorderWidth();
+        this._initDataPrompt();
+
+        this.viewBody = $("<div class='ui-card-view-body' />");
+        this.element.append(this.viewBody);
+        this._initPagerPanel();
+
+        this.setSize(this.option.width, this.option.height);
+        // 修正selection设置项
+        if(!this.option.selection) {
+            this.option.selection = false;
+        } else {
+            if(this.option.selection.type === "disabled") {
+                this.option.selection = false;
+            }
+        }
+
+        if(Array.isArray(this.option.viewData)) {
+            this.fill(this.option.viewData, this.option.viewData.length);
+        }
+
+        // 项目选中处理程序
+        this.onBodyClickHandler = $.proxy(onBodyClick, this);
+    },
+    _initBorderWidth: function() {
+        var getBorderWidth = function(key) {
+            return parseInt(this.element.css(key), 10) || 0;
+        };
+        this.borderWidth = 0;
+        this.borderHeight = 0;
+
+        this.borderWidth += getBorderWidth.call(this, "border-left-width");
+        this.borderWidth += getBorderWidth.call(this, "border-right-width");
+
+        this.borderHeight += getBorderWidth.call(this, "border-top-width");
+        this.borderHeight += getBorderWidth.call(this, "border-bottom-width");
+    },
+    _initDataPrompt: function() {
+        var text;
+        if(this._hasPrompt) {
+            this._dataPrompt = $("<div class='data-prompt' />");
+            text = this.option.promptText;
+            if (ui.core.isString(text) && text.length > 0) {
+                this._dataPrompt.html("<span class='font-highlight'>" + text + "</span>");
+            } else if (ui.core.isFunction(text)) {
+                text = text();
+                this._dataPrompt.append(text);
+            }
+            this.gridBody.append(this._dataPrompt);
+        }
+    },
+    _initPagerPanel: function() {
+        if(this.pager) {
+            this.gridFoot = $("<div class='ui-card-view-foot clear' />");
+            this.element.append(this.gridFoot);
+            
+            this.pager.pageNumPanel = $("<div class='page-panel' />");
+            if (this.option.pager.displayDataInfo) {
+                this.pager.pageInfoPanel = $("<div class='data-info' />");
+                this.gridFoot.append(this.pager.pageInfoPanel)
+            } else {
+                this.pager.pageNumPanel.css("width", "100%");
+            }
+
+            this.gridFoot.append(this.pager.pageNumPanel);
+            this.pager.pageChanged(function(pageIndex, pageSize) {
+                this.pageIndex = pageIndex;
+                this.pageSize = pageSize;
+                this.fire("pagechanging", pageIndex, pageSize);
+            }, this);
+        }
+    },
+    _rasterizeItems: function(arr, fn) {
+        var marginInfo,
+            i, j,
+            index,
+            top, left,
+            isFunction,
+            rows;
+
+        if(arr.length == 0) {
+            return;
+        }
+        marginInfo = this._getMargin(arr.length);
+        this._columnCount = marginInfo.count;
+        if(marginInfo.count == 0) return;
+        
+        isFunction = ui.core.isFunction(fn);
+        rows = Math.floor((arr.length + marginInfo.count - 1) / marginInfo.count);
+        this.viewPanel.css("height", (rows * (this.option.itemHeight + marginInfo.margin) + marginInfo.margin) + "px");
+        for(i = 0; i < rows; i++) {
+            for(j = 0; j < marginInfo.count; j++) {
+                index = (i * marginInfo.count) + j;
+                if(index >= arr.length) {
+                    return;
+                }
+                top = (i + 1) * marginInfo.margin + (i * this.option.itemHeight);
+                left = (j + 1) * marginInfo.margin + (j * this.option.itemWidth);
+                if(isFunction) {
+                    fn.call(this, arr[index], index, top, left);
+                }
+            }
+        }
+    },
+    _getMargin: function(length, scrollHeight) {
+        var currentWidth = this.viewBody.width(),
+            currentHeight = this.viewBody.height(),
+            result,
+            restWidth,
+            flag,
+            checkOverflow = function(len, res) {
+                if(res.count == 0) {
+                    return res;
+                }
+                var sh = Math.floor((len + res.count - 1) / res.count) * (res.margin + this.option.itemHeight) + res.margin;
+                if(sh > currentHeight) {
+                    return this._getMargin(len, sh);
+                } else {
+                    return res;
+                }
+            };
+        if(scrollHeight) {
+            flag = true;
+            if(scrollHeight > currentHeight) {
+                currentWidth -= ui.scrollbarWidth;
+            }
+        }
+        result = {
+            count: Math.floor(currentWidth / this.option.itemWidth),
+            margin: 0
+        };
+        restWidth = currentWidth - result.count * this.option.itemWidth;
+        result.margin = Math.floor(restWidth / (result.count + 1));
+        if(result.margin >= 3) {
+            return flag ? result : checkOverflow.call(this, length, result);
+        }
+        result.margin = 3;
+
+        result.count = Math.floor((currentWidth - ((result.count + 1) * result.margin)) / this.option.itemWidth);
+        restWidth = currentWidth - result.count * this.option.itemWidth;
+        result.margin = Math.floor(restWidth / (result.count + 1));
+
+        return flag ? result : checkOverflow.call(this, length, result);
+    },
+    _createItem: function(itemData, index) {
+        var div = $("<div class='view-item' />");
+        div.css({
+            "width": this.option.itemWidth + "px",
+            "height": this.option.itemHeight + "px"
+        });
+        div.attr("data-index", index);
+        return div;
+    },
+    _renderItem: function(itemElement, itemData, index) {
+        var elem, frame, formatter;
+
+        formatter = this.option.renderItemFormatter;
+        if(ui.core.isFunction(formatter)) {
+            elem = formatter.call(this, itemData, index);
+            if(elem) {
+                itemElement.append(elem);
+            }
+        }
+        frame = $("<div class='frame-panel border-highlight'/>");
+        frame.css({
+            "width": this.option.itemWidth - (frameBorderWidth * 2) + "px",
+            "height": this.option.itemHeight - (frameBorderWidth * 2) + "px"
+        });
+        itemElement.append(frame);
+        itemElement.append("<i class='check-marker border-highlight'></i>");
+        itemElement.append("<i class='check-icon fa fa-check'></i>");
+    },
+    _renderPageList: function(rowCount) {
+        if (!this.pager) {
+            return;
+        }
+        this.pager.data = this.option.viewData;
+        this.pager.pageIndex = this.pageIndex;
+        this.pager.pageSize = this.pageSize;
+        this.pager.renderPageList(rowCount);
+    },
+    _recomposeItems: function() {
+        var arr;
+        if(!this.bodyPanel)
+            return;
+        
+        arr = this.bodyPanel.children();
+        this._rasterizeItems(arr, function(item, index, top, left) {
+            $(item).css({
+                "top": top + "px",
+                "left": left + "px"
+            });
+        });
+    },
+    _getSelectionData: function(elem) {
+        var index,
+            data,
+            viewData;
+
+        index = parseInt(elem.attr("data-index"), 10);
+        viewData = this.getViewData();
+        data = {
+            itemIndex: index,
+            itemData: viewData[index]
+        };
+        return data;
+    },
+    _selectItem: function(elem) {
+        var eventData,
+            result,
+            i, len;
+
+        eventData = this._getSelectionData(elem);
+        eventData.itemElement = elem;
+        eventData.originElement = elem.context ? $(elem.context) : null;
+
+        result = this.fire("selecting", eventData);
+        if(result === false) {
+            return;
+        }
+
+        if(this.isMultiple()) {
+            if(elem.hasClass(selectedClass)) {
+                for(i = 0, len = this._selectList.length; i < len; i++) {
+                    if(this._selectList[i] === elem[0]) {
+                        this._selectList.splice(i, 1);
+                        break;
+                    }
+                }
+                elem.removeClass(selectedClass);
+                this.fire("deselected", eventData);
+            } else {
+                this._selectList.push(elem[0]);
+                elem.addClass(selectedClass);
+                this.fire("selected", eventData);
+            }
+        } else {
+            if(this._current) {
+                this._current.removeClass(selectedClass);
+                if(this_current[0] === elem[0]) {
+                    this._current = null;
+                    this.fire("deselected", eventData);
+                    return;
+                }
+                this._current = elem;
+                elem.addClass(selectedClass);
+                this.fire("selected", eventData);
+            }
+        }
+    },
+    _promptIsShow: function() {
+        return this._hasPrompt 
+            && this._dataPrompt.css("display") === "block";
+    },
+    _setPromptLocation: function() {
+        var height = this._dataPrompt.height();
+        this._dataPrompt.css("margin-top", -(height / 2) + "px");
+    },
+    _showDataPrompt: function() {
+        if(!this._hasPrompt) return;
+        this._dataPrompt.css("display", "block");
+        this._setPromptLocation();
+    },
+    _hideDataPrompt: function() {
+        if(!this._hasPrompt) return;
+        this._dataPrompt.css("display", "none");
+    },
+
+    /// API
+    /** */
+    fill: function(viewData, rowCount) {
+        var isRebind;
+
+        isRebind = false;
+        if(!this.bodyPanel) {
+            this.bodyPanel = $("<div class='ui-card-view-panel'/>");
+            if(this.option.selection)
+                this.bodyPanel.click(this.onBodyClickHandler);
+            this.viewBody.append(this.bodyPanel);
+        } else {
+            this.viewBody.scrollTop(0);
+            this.clear(false);
+            isRebind = true;
+        }
+
+        if(!Array.isArray(viewData)) {
+            viewData = [];
+        }
+        this.option.viewData = viewData;
+
+        if(viewData.length == 0) {
+            this._showDataPrompt();
+            return;
+        } else {
+            this._hideDataPrompt();
+        }
+
+        this._rasterizeItems(viewData, function(itemData, index, top, left) {
+            var elem = this._createItem(itemData, index);
+            elem.css({
+                "top": top + "px",
+                "left": left + "px"
+            });
+            this._renderItem(elem, itemData, index);
+            this.bodyPanel.append(elem);
+        });
+
+        //update page numbers
+        if ($.isNumeric(rowCount)) {
+            this._renderPageList(rowCount);
+        }
+
+        if (isRebind) {
+            this.fire("rebind");
+        }
+    },
+    getSelection: function() {
+        var result,
+            i, len;
+        if(!this.isSelectable()) {
+            return null;
+        }
+
+        if(this.isMultiple()) {
+            result = [];
+            for(i = 0, len = this._selectList.length; i < len; i++) {
+                result.push(this._getSelectionData($(this._selectList[i])));
+            }
+        } else {
+            result = null;
+            if(this._current) {
+                result = this._getSelectionData(this._current);
+            }
+        }
+        return result;
+    },
+    cancelSelection: function() {
+        var i, len;
+        if(!this.isSelectable()) {
+            return;
+        }
+
+        if(this.isMultiple()) {
+            if(this._selectList.length === 0) {
+                return;
+            }
+            for(i = 0, len = this._selectList.length; i < len; i++) {
+                elem = $(this._selectList[i]);
+                fn.call(this, elem);
+            }
+            this._selectList = [];
+        } else {
+            if(!this._current) {
+                return;
+            }
+            fn.call(this, this._current);
+            this._current = null;    
+        }
+        this.fire("cancel");
+    },
+    /** 获取视图数据 */
+    getViewData: function() {
+        return Array.isArray(this.option.viewData) 
+            ? this.option.viewData 
+            : [];
+    },
+    /** 获取当前尺寸下一行能显示多少个元素 */
+    getColumnCount: function() {
+        return this._columnCount;
+    },
+    /** 获取项目数 */
+    count: function() {
+        return Array.isArray(this.option.viewData)
+            ? 0
+            : this.option.viewData.length;
+    },
+    /** 是否可以选择 */
+    isSelectable: function() {
+        return !!this.option.selection;
+    },
+    /** 是否支持多选 */
+    isMultiple: function() {
+        return this.option.selection.multiple === true;
+    },
+    /** 清空表格数据 */
+    clear: function() {
+        if (this.bodyPanel) {
+            this.bodyPanel.html("");
+            this.option.viewData = [];
+            this._selectList = [];
+            this._current = null;
+        }
+        if (this.pager) {
+            this.pager.empty();
+        }
+        if (arguments[0] !== false) {
+            this._showDataPrompt();
+        }
+    },
+    /** 设置表格的尺寸, width: 宽度, height: 高度 */
+    setSize: function(width, height) {
+        var needRecompose = false;
+        if(arguments.length === 1) {
+            height = width;
+            width = null;
+        }
+        if(ui.core.isNumber(height)) {
+            height -= this.columnHeight + this.borderHeight;
+            if(this.pager) {
+                height -= this.pagerHeight;
+            }
+            this.gridBody.css("height", height + "px");
+            needRecompose = true;
+        }
+        if(ui.core.isNumber(width)) {
+            width -= this.borderWidth;
+            this.element.css("width", width + "px");
+            needRecompose = true;
+        }
+        if(needRecompose) {
+            this._recomposeItems();
+        }
+        if(this._promptIsShow()) {
+            this._setPromptLocation();
+        }
+    }
+});
