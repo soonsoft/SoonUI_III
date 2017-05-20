ca475a424ba005373e55f8f6541a9c0c43558143
soonsoft <soonz@aliyun.com>
2017-05-20 17:27:36 +0800

    Create selection-tree.js

---
+++ ui/control/select/selection-tree.js
@@ -0,0 +1,361 @@
+/**
+ * 树形下拉列表，可以完美的解决多级联动下拉列表的各种弊端
+ * 支持单项选择和多项选择
+ */
+
+var selectedClass = "ui-selection-tree-selected",
+    checkboxClass = "ui-selection-tree-checkbox",
+    flodClass = "fold-button",
+    unflodClass = "unfold-button";
+
+var instanceCount = 0,
+    parentNode = "_selectionTreeParentNode";
+
+var flodButtonLeft = 3,
+    flodButtonWidth = 14;
+
+function getParent() {
+    return this[parentNode];
+}
+// 获取值
+function getValue(field) {
+    var value,
+        arr,
+        i, len;
+    
+    arr = field.split(".");
+    value = this[arr[i]];
+    for(i = 1, len = arr.length; i < len; i++) {
+        value = value[arr[i]];
+        if(value === undefined || value === null) {
+            break;
+        }
+    }
+    if(value === undefined) {
+        value = null;
+    }
+    return value;
+}
+// 获取多个字段的值并拼接
+function getArrayValue(fieldArray) {
+    var result = [],
+        value,
+        i = 0;
+    for(; i < fieldArray.length; i++) {
+        value = this[fieldArray[i]];
+        if(value === undefined) {
+            value = null;
+        }
+        result.push(value + "");
+    }
+    return result.join("_");
+}
+function defaultItemFormatter(text, marginLeft, item) {
+    var span = $("<span />").text(text);
+    if(marginLeft > 0) {
+        span.css("margin-left", marginLeft);
+    }
+    return span;
+}
+function setChecked(cbx, checked) {
+    if(checked) {
+        cbx.removeClass("fa-square")
+            .addClass("fa-check-square").addClass("font-highlight");
+    } else {
+        cbx.removeClass("fa-check-square").removeClass("font-highlight")
+            .addClass("fa-square");
+    }
+}
+
+// 事件处理函数
+// 树节点点击事件
+function onTreeItemClick(e) {
+    var elem;
+
+    if(this.isMultiple()) {
+        e.stopPropagation();
+    }
+
+    elem = $(e.target);
+    if(elem.hasClass(foldClass)) {
+        this.onTreeFoldClickHandler(e);
+        return;
+    }
+
+    // TODO
+}
+// 折叠按钮点击事件
+function onTreeFoldClick(e) {
+
+}
+// 异步状态点击折叠按钮事件
+function onTreeFoldLazyClick(e) {
+
+}
+
+ui.define("ui.ctrls.SelectionTree", {
+    _defineOption: function() {
+        return {
+            // 是否支持多选
+            multiple: false,
+            // 获取值的属性名称，可以用多个值进行组合，用数组传入["id", "name"], 可以支持子属性node.id，可以支持function
+            valueField: null,
+            // 获取文字的属性名称，可以用多个值进行组合，用数组传入["id", "name"], 可以支持子属性node.id，可以支持function
+            textField: null,
+            // 获取父节点的属性名称，可以用多个值进行组合，用数组传入["id", "name"], 可以支持子属性node.id，可以支持function
+            parentField: null,
+            // 子节点的属性名称，子节点为数组，和parentField互斥，如果两个值都设置了，优先使用childField
+            childField: null,
+            // 视图数据
+            viewData: null,
+            // 是否只能选择叶节点
+            nodeSelectable: false,
+            // 默认展开的层级，false|0：显示第一层级，true：显示所有层级，数字：显示的层级值(0表示根级别，数值从1开始)
+            defaultExpandLevel: false,
+            // 是否延迟加载，只有用户展开这个节点才会渲染节点下面的数据（对大数据量时十分有效）
+            lazy: false,
+            // 内容格式化器，可以自定义内容
+            itemFormatter: null
+        };
+    },
+    _defineEvents: function() {
+        return ["selecting", "selected", "cancel"];
+    },
+    _create: function() {
+        var fields, fieldMethods;
+
+        this._super();
+        this._current = null;
+        this._selectList = [];
+        this._treePrefix = "selectionTree_" + (++instanceCount) + "_";
+        
+        fields = [this.option.valueField, this.option.textField, this.option.parentField];
+        fieldMethods = ["_getValue", "_getText", "_getParent"];
+        fields.forEach(function(item) {
+            if(Array.isArray(item, index)) {
+                this[fieldMethods[index]] = getArrayValue;
+            } else if($.isFunction(item)) {
+                this[fieldMethods[index]] = item;
+            } else {
+                this[fieldMethods[index]] = getValue;
+            }
+        }, this);
+
+        if(this.option.defaultExpandLevel === false) {
+            this.expandLevel = 0;
+        } else {
+            if(ui.core.isNumber(this.option.defaultExpandLevel)) {
+                this.expandLevel = 
+                    this.option.defaultExpandLevel <= 0
+                        ? 0 
+                        : this.option.defaultExpandLevel;
+            } else {
+                // 设置到最大展开1000层
+                this.expandLevel = 1000;
+            }
+        }
+
+        if(this.option.lazy) {
+            if(ui.core.isFunction(this.option.lazy.hasChildren)) {
+                this._hasChildren = this.option.lazy.hasChildren;
+            }
+            if(ui.core.isFunction(this.option.lazy.loadChildren)) {
+                this._loadChildren = this.option.lazy.loadChildren;
+                // 当数据延迟加载是只能默认加载根节点
+                this.expandLevel = 0;
+            }
+
+            this.onTreeFoldClickHandler = $.proxy(onTreeFoldLazyClick, this);
+            this.option.lazy = true;
+        } else {
+            this.onTreeFoldClickHandler = $.proxy(onTreeFoldClick, this);
+            this.option.lazy = false;
+        }
+
+        if(!ui.core.isFunction(this.option.itemFormatter)) {
+            this.option.itemFormatter = defaultItemFormatter;
+        }
+
+        this.onTreeItemClickHandler = $.proxy(onTreeItemClick, this);
+
+        this._init();
+    },
+    
+    _init: function() {
+        this.treePanel = $("<div class='ui-selection-tree-panel border-highlight' />");
+        this.treePanel.click(this.onTreeItemClickHandler);
+        this.wrapElement(this.element, this.treePanel);
+
+        this._showClass = "ui-selection-tree-show";
+        this._clearClass = "ui-selection-tree-clear";
+        this._clear = function() {
+            this.cancelSelection();
+        };
+        this._selectTextClass = "select-text";
+
+        this.initPanelWidth(this.option.width);
+        if (ui.core.isFunction(this.option.itemFormatter)) {
+            this.itemFormatter = this.option.itemFormatter;
+        } else {
+            this.itemFormatter = defaultItemFormatter;
+        }
+        if (Array.isArray(this.option.viewData)) {
+            this._fill(this.option.viewData);
+        }
+        this._super();
+    },
+    _fill: function (data) {
+        var dl,
+            viewData;
+
+        this.clear();
+
+        dl = $("<dl class='ui-selection-tree-dl' />");
+        viewData = this.getViewData();
+        if (this.option.childField) {
+            this._renderTree(viewData, dl, 0);
+        } else if (this.option.parentField) {
+            this._listDataToTree(viewData, dl, 0);
+        }
+        this.treePanel.append(dl);
+    },
+    _listDataToTree: function(viewData, dl, level) {
+        var childField;
+        if(!Array.isArray(viewData) || viewData.length === 0) {
+            return;
+        }
+
+        this.originalViewData = viewData;
+        childField = "children";
+        viewData = ui.trans.listToTree(
+            viewData, 
+            this.option.parentField, 
+            this.option.valueField, 
+            childField);
+        if(viewData) {
+            this.option.childField = childField;
+            this.option.viewData = viewData;
+            this._renderTree(viewData, dl, level);
+        } else {
+            delete this.originalViewData;
+        }
+    },
+    _renderTree: function(viewData, dl, level, idValue, parentData) {
+        var id, text, children,
+            item, i, len, tempMargin,
+            childDL, dt, dd, cbx,
+            path;
+        if(!Array.isArray(viewData) || viewData.length === 0) {
+            return;
+        }
+
+        path = idValue;
+        for(i = 0, len = viewData.length; i < len; i++) {
+            tempMargin = 0;
+            item = viewData[i];
+            item[parentNode] = parentData || null;
+            item.getParent = getParent;
+
+            id = path ? (path + "_" + i) : ("" + i);
+            text = this._getText.call(item, this.option.textField) || "";
+
+            dt = $("<dt class='ui-selection-tree-dt' />");
+            if(this.isMultiple()) {
+                cbx = this._createCheckbox();
+            }
+            dt.prop("id", id);
+            dl.append(dt);
+
+            if(this._hasChildren(item)) {
+                children = this._getChildren(item);
+                dd = $("<dd class='ui-selection-tree-dd' />");
+
+                if(level + 1 <= this.expandLevel) {
+                    dt.append(this._createFoldButton(level, unflodClass, "fa-angle-down"));
+                } else {
+                    dt.append(this._createFoldButton(level, "fa-angle-right"));
+                    dd.css("display", "none");
+                }
+                if(this.option.nodeSelectable === true && cbx) {
+                    dt.append(cbx);
+                }
+
+                if(this.option.lazy) {
+                    if(level + 1 <= this.expandLevel) {
+                        this._loadChildren(dt, dd, item);
+                    }
+                } else {
+                    childDL = $("<dl class='ui-selection-tree-dl' />");
+                    this._renderTree(children, childDL, level + 1, id, item);
+                    dd.append(childDL);
+                }
+                dl.append(dd);
+            } else {
+                tempMargin = (level + 1) * (flodButtonWidth + flodButtonLeft) + flodButtonLeft;
+                if(cbx) {
+                    cbx.css("margin-left", tempMargin + flodButtonLeft + "px");
+                    tempMargin = 0;
+                    dt.append(cbx);
+                }
+            }
+            dt.append(
+                this.option.itemFormatter.call(this, text, tempMargin, item, dt));
+        }
+    },
+    _createCheckbox: function() {
+        var checkbox = $("<i class='fa fa-square' />");
+        checkbox.addClass(checkboxClass);
+        return checkbox;
+    },
+    _createFoldButton: function(level) {
+        var btn, i, len;
+        
+        btn = $("<i class='fold-button font-highlight-hover fa' />");
+        for (i = 1, len = arguments.length; i < len; i++) {
+            btn.addClass(arguments[i]);
+        }
+        btn.css("margin-left", (level * (flodButtonWidth + flodButtonLeft) + flodButtonLeft) + "px");
+        return btn;
+    },
+    _getChildren: function (treeNode) {
+        return treeNode[this.option.childField];
+    },
+    _hasChildren: function(treeNode) {
+        var children = this._getChildren(treeNode);
+        return Array.isArray(children) && children.length > 0;
+    },
+    _loadChildren: function(dt, dd, treeNode) {
+        var children = this._getChildren(treeNode);
+        this._appendChildren(dt, dd, treeNode, children);
+    },
+
+    /// API
+    /** 设置视图数据 */
+    setViewData: function(data) {
+        if(Array.isArray(data)) {
+            this._fill(data);
+        }
+    },
+    /** 获取视图数据 */
+    getViewData: function() {
+        return Array.isArray(this.option.viewData) 
+            ? this.option.viewData 
+            : [];
+    },
+    /** 获取项目数 */
+    count: function() {
+        return this.viewData.length;
+    },
+    /** 是否可以多选 */
+    isMultiple: function() {
+        return this.option.multiple === true;
+    },
+    /** 清空列表 */
+    clear: function() {
+        this.option.viewData = [];
+        this.listPanel.empty();
+        this._current = null;
+        this._selectList = [];
+        delete this.originalViewData;
+    }
+});
