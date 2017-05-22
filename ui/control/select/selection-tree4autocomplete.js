624e72e9db893812a294fa2af584afcfda041f56
soonsoft <soonz@aliyun.com>
2017-05-22 08:58:20 +0800

    Create selection-tree4autocomplete.js

---
+++ ui/control/select/selection-tree4autocomplete.js
@@ -0,0 +1,276 @@
+
+/**
+ * 支持自动完成的下拉树
+ */
+
+var selectedClass = "autocomplete-selected";
+
+function onFocus(e) {
+    ui.hideAll(this);
+    this._resetTreeList();
+    this.show();
+}
+function onKeyup(e) {
+    if(e.which === ui.keyCode.DOWN) {
+        this._moveSelection(1);
+    } else if(e.which === ui.keyCode.UP) {
+        this._moveSelection(-1);
+    } else if(e.which === ui.keyCode.ENTER) {
+        this._selectCompleter();
+    }
+}
+function onMouseover(e) {
+    var elem = $(e.target),
+        nodeName;
+
+    while((nodeName = elem.nodeName()) !== "DT" 
+            && !elem.hasClass("autocomplete-dt")) {
+        
+        if(elem.hasClass("autocomplete-dl")) {
+            return;
+        }
+        elem = elem.parent();
+    }
+    if(this._currentCompleterElement) {
+        this._currentCompleterElement.removeClass(selectedClass);
+    }
+    this._currentCompleterElement = elem;
+    this._currentCompleterElement.addClass(selectedClass);
+}
+function onClick(e) {
+    e.stopPropagation();
+    this._selectCompleter();
+}
+function onTextinput(e) {
+    var elem = $(e.target),
+        value = elem.val(),
+        oldValue = elem.data("autocomplete.value");
+    if(this._cancelAutoComplete) {
+        return;
+    }
+    if(value.length === 0) {
+        this._resetTreeList();
+        this.cancelSelection();
+        return;
+    }
+    if(this._autoCompleteListIsShow() && oldValue === value) {
+        return;
+    }
+    elem.data("autocomplete.value", value);
+    if(!this.isShow()) {
+        this.show();
+    }
+    this._launch(value);
+}
+
+
+ui.define("ui.ctrls.AutocompleteSelectionTree", ui.ctrls.SelectionTree, {
+    _create: function() {
+        // 只支持单选
+        this.option.multiple = false;
+        // 设置最小结果显示条数，默认是10条
+        if(!ui.core.isNumber(this.option.limit)) {
+            this.option.limit = 10;
+        } else {
+            if(this.option.limit <= 0) {
+                this.option.limit = 10;
+            } else if(this.option.limit > 100) {
+                this.option.limit = 100;
+            }
+        }
+
+        // 初始化事件处理函数
+        this.onFocusHandler = $.proxy(onFocus, this);
+        this.onKeyupHandler = $.proxy(onKeyup, this);
+        this.onMouseoverHandler = $.proxy(onMouseover, this);
+        this.onClickHandler = $.proxy(onClick, this);
+        this.onTextinputHandler = $.proxy(onTextinput, this);
+
+        this._super();
+    },
+    _init: function() {
+        var oldFireFn;
+
+        this._super();
+        this.element
+            .off("focus")
+            .on("focus", this.onFocusHandler)
+            .on("keyup", this.onKeyupHandler);
+
+        if(ui.browser.ie && ui.browser < 9) {
+            oldFireFn = this.fire;
+            this.fire = function() {
+                this._callAndCancelPropertyChange(oldFireFn, arguments);
+            };
+        }
+        this.element.textinput(this.onTextinputHandler);
+        this._clear = function() {
+            this.cancelSelection(true, this._autoCompleteListIsShow());
+        };
+    },
+    _callAndCancelPropertyChange: function(fn, args) {
+        //修复IE8下propertyChange事件由于用户赋值而被意外触发
+        this._cancelAutoComplete = true;
+        fn.apply(this, args);
+        this._cancelAutoComplete = false;
+    },
+    _launch: function(searchText) {
+        var viewData = this.getViewData(),
+            response;
+        if(viewData.length === 0) {
+            return;
+        }
+        this.cancelSelection(false, false);
+        response = this._search(searchText, viewData, this.option.limit);
+        this._showSearchInfo(response, searchText);
+    },
+    _search: function(searchText, viewData, limit) {
+        var beginArray = [], 
+            containArray = [],
+            result;
+        
+        searchText = searchText.toLowerCase();
+        this._doSearch(beginArray, containArray, searchText, viewData, limit);
+        result = beginArray.concat(containArray);
+        return result.slice(0, limit);
+    },
+    _doSearch: function(beginArray, containArray, searchText, viewData, limit, path) {
+        var i, len, 
+            nodeData, id;
+        
+        for(i = 0, len = viewData.length; i < len; i++) {
+            if(beginArray.length > limit) {
+                return;
+            }
+            id = path ? (path + "_" + i) : ("" + i);
+            nodeData = data[i];
+            if(this._hasChildren(nodeData)) {
+                if(this.option.nodeSelectable === true) {
+                    this._doQuery(beginArray, containArray, searchText, nodeData, id);
+                }
+                this._doSearch(beginArray, containArray, searchText, this._getChildren(nodeData), limit, id);
+            } else {
+                this._doQuery(beginArray, containArray, searchText, nodeData, id);
+            }
+        }
+    },
+    _doQuery: function(beginArray, containArray, searchText, nodeData, path) {
+        var index;
+        index = this._getText.call(nodeData, this.option.textField)
+                    .toLowerCase()
+                    .search(searchText);
+        if(index === 0) {
+            beginArray.push({ nodeData: nodeData, path: path });
+        } else if(index > 0) {
+            containArray.push({ nodeData: nodeData, path: path });
+        }
+    },
+    _showSearchInfo: function(info, searchText) {
+        var dl, html, textHtml, 
+            regexp, hintHtml,
+            i, len;
+        
+        dl = this._autoCompleteList;
+        if(!dl) {
+            dl = this._autoCompleteList = $("<dl class='autocomplete-dl' />");
+            dl.hide();
+            dl.click(this.onClickHandler)
+                .mouseover(this.onMouseoverHandler);
+            this.treePanel.append(dl);
+        } else {
+            dl.empty();
+        }
+
+        html = [];
+        regexp = new RegExp(searchText, "gi");
+        hintHtml = "<span class='font-highlight'>" + searchText + "</span>";
+        for(i = 0, len = info.length; i < len; i++) {
+            html.push("<dt data-path='" + info[i].path + "'>");
+            html.push("<span class='normal-text'>");
+            textHtml = this._getText.call(info[i].nodeData, this.option.textField);
+            textHtml = textHtml.replace(re, hintHtml);
+            html.push(textHtml);
+            html.push("</span></dt>");
+        }
+        $(this.treePanel.children()[0]).hide();
+        dl.append(html.join(""));
+        dl.show();
+        this._moveSelection(1);
+    },
+    _autoCompleteListIsShow: function() {
+        if(this._autoCompleteList) {
+            return this._autoCompleteList.css("display") === "block";
+        } else {
+            return false;
+        }
+    },
+    _resetTreeList: function() {
+        var children = this.treePanel.children();
+        $(children[1]).hide();
+        $(children[0]).show();
+    },
+    _selectCompleter: function() {
+        var path, nodeData, dt;
+        if(this._currentCompleterElement) {
+            path = this._currentCompleterElement.attr("data-path");
+            nodeData = this._getNodeDataByPath(path);
+            if (nodeData) {
+                dt = this._selectNodeByValue(nodeData, path);
+                //触发选择事件
+                this.fire("selected", dt, this._getSelectionData(this._getNodeData(dt)));
+            }
+            ui.hideAll();
+        }
+    },
+    _moveSelection: function(step) {
+        var children,
+            elem;
+
+        children = $(this.treePanel.children()[1]).children();
+        if(!this._currentCompleterElement) {
+            this._currentCompleterElement = $(children[0]);
+        } else {
+            this._currentCompleterElement.removeClass(selectedClass);
+        }
+
+        if(step === 0) {
+            this._currentCompleterElement = $(children[0]);
+        } else if(step === 1) {
+            elem = this._currentCompleterElement.next();
+            if(elem.length === 0) {
+                elem = $(children[0]);
+            }
+            this._currentCompleterElement = elem;
+        } else if(step === -1) {
+            elem = this._currentCompleterElement.prev();
+            if(elem.length === 0) {
+                elem = $(children[children.length - 1]);
+            }
+            this._currentCompleterElement = elem;
+        }
+        this._currentCompleterElement.addClass(selectedClass);
+    },
+    _selectItem: function() {
+        this._callAndCancelPropertyChange(this._super, arguments);
+    },
+
+    // API
+    /** 取消选中 */
+    cancelSelection: function(isFire, justAutoCompleteListCancel) {
+        if(justAutoCompleteListCancel) {
+            this._callAndCancelPropertyChange(function() {
+                this.element.val("");
+            });
+            this._resetTreeList();
+        } else {
+            this._super(isFire);
+        }
+    }
+});
+
+$.fn.autocompleteSelectionTree = function(option) {
+    if(this.length === 0) {
+        return null;
+    }
+    return ui.ctrls.autocompleteSelectionTree(option, this);
+};
