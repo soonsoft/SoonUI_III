2c3144738af8f7ea6fa3e0344b531afb0719b81c
soonsoft <soonz@aliyun.com>
2017-05-22 09:16:09 +0800

    Update tree-view.js

--- ui/control/view/tree-view.js
+++ ui/control/view/tree-view.js
@@ -1,1 +1,23 @@
-// tree-view
+
+/**
+ * 树形列表
+ */
+
+ui.define("ui.ctrls.TreeView", {
+    _init: function() {
+        var position;
+
+        this.treePanel = this.element;
+        position = this.treePanel.css(position);
+        
+        this.treePanel
+            .addClass("ui-selection-tree-panel")
+            .addClass("ui-tree-view-panel")
+            .css("position", position);
+        this.treePanel.click(this.onTreeItemClickHandler);
+
+        if (Array.isArray(this.option.viewData)) {
+            this._fill(this.option.viewData);
+        }
+    }
+});
