0c671ed44248dff327bb5a82377249d852ca95df
soonsoft <soonz@aliyun.com>
2017-06-05 10:45:20 +0800

    Update option-box.js

--- ui/control/box/option-box.js
+++ ui/control/box/option-box.js
@@ -1,1 +1,117 @@
 // OptionBox
+var contentTop = 40,
+    buttonTop = 0,
+    buttonPanelHeight = 0;
+ui.define("ui.ctrls.OptionBox", ui.ctrls.SidebarBase, {
+    _defineOption: function() {
+        return $.extend(this._super(), {
+            title: "",
+            buttons: null
+        });
+    },
+    _create: function() {
+        this._super();
+        this.contentPanel = null;
+        this.contentHeight = 0;
+        this.contentTop = 40;
+        this.buttonTop = 0;
+        this.buttonPanelHeight = 0;
+
+        this.buttons = [];
+        if(!Array.isArray(this.option.buttons)) {
+            if(ui.core.isString(this.option.buttons)) {
+                this.option.buttons = [this.option.buttons];
+            } else {
+                this.option.buttons = [];
+            }
+        }
+
+        this.opacityOption = {
+            target: this.panel,
+            ease: ui.AnimationStyle.easeFromTo,
+            onChange: function(val, elem) {
+                elem.css("opacity", val / 100);
+            }
+        };
+    },
+    _render: function() {
+        this._super();
+
+        this._panel.addClass("ui-option-box-panel");
+        this.titlePanel = $("<section class='ui-option-box-title' />");
+        this.contentPanel = $("<section class='ui-option-box-content' />");
+
+        this.contentPanel.append(this.element);
+        this.contentHeight = this.element.height();
+
+        this._panel
+            .append(this.titlePanel)
+            .append(this.contentPanel);
+        this._initButtons();
+        this.setTitle(this.option.title);
+    },
+    _initButtons: function() {
+        var i, len;
+        for(i = 0, len = this.option.buttons.length; i < len; i++) {
+            this.addButton(this.option.buttons[i]);
+        }
+    },
+    addButton: function(button) {
+        var buttonContainer;
+        button = ui.getJQueryElement(button);
+        if(!button) {
+            return;
+        }
+        this.buttons.push(button);
+        if(!this.buttonPanel) {
+            this.buttonPanel = $("<section class='ui-option-box-buttons' />");
+            buttonContainer = $("<div class='ui-form' />");
+            this.buttonPanel.append(buttonContainer);
+            this._panel.append(this.buttonPanel);
+            this.buttonTop = 24;
+            this.buttonPanelHeight = 24;
+        } else {
+            buttonContainer = this.buttonPanel.children(".ui-form");
+        }
+        buttonContainer.append(button);
+        return this;
+    },
+    setTitle: function(title) {
+        this.titlePanel.empty();
+        if(title) {
+            if(ui.core.isString(title)) {
+                title = "<span class='option-box-title-text font-highlight'>" + title + "<span>";
+            }
+            this.titlePanel.append(title);
+        }
+    },
+    setWidth: function(width) {
+        var contentMaxheight;
+
+        this._super(width);
+        // 调整内容的对齐方式
+        contentMaxheight = this.height - this.contentTop - this.buttonTop - this.buttonPanelHeight - this.buttonTop;
+        this.contentPanel.css({
+            "max-height": contentMaxheight + "px"
+        });
+        if (this.buttonPanel) {
+            if (contentMaxheight < this.contentHeight) {
+                this.buttonPanel.css("width", this.width - ui.scrollbarWidth + "px");
+            } else {
+                this.buttonPanel.css("width", "100%");
+            }
+        }
+    },
+    show: function() {
+        this.showTimeValue = 240;
+        this.opacityOption.begin = 0;
+        this.opacityOption.end = 100;
+        this._super(this.opacityOption);
+    },
+    hide: function() {
+        this.hideTimeValue = 240;
+        this.opacityOption.begin = 100;
+        this.opacityOption.end = 0;
+        this._super(this.opacityOption);
+    }
+});
