b3ae7dbae37fa3455536c4138ae187aea21b77f4
soonsoft <soonz@aliyun.com>
2017-06-29 11:50:30 +0800

    Update theme.js

--- ui/theme.js
+++ ui/theme.js
@@ -1,4 +1,19 @@
 
+function setHighlight(color) {
+    var sheet,
+        styleUrl;
+    sheet = $("#" + ui.theme.themeSheetId);
+    if(sheet.length > 0) {
+        styleUrl = sheet.prop("href");
+        styleUrl = ui.url.setParams({
+            themeId: color.Id
+        });
+        sheet.prop("href", styleUrl);
+    }
+    ui.theme.currentTheme = color;
+    ui.page.fire("themeChanged", color);
+}
+
 //主题
 ui.theme = {
     /** 当前的主题背景色 */
@@ -26,30 +41,24 @@ ui.theme = {
         }
         return themeInfo;
     },
-    /** 修改主题 */
+    /** 修改高亮色 */
     changeHighlight: function(url, color) {
         ui.ajax.ajaxPost(url, 
             { themeId: color.Id },
             function(success) {
-                var sheet,
-                    url,
-                    urlObj;
                 if(success.Result) {
-                    sheet = $("#" + ui.theme.themeSheetId);
-                    if(sheet.length > 0) {
-                        url = sheet.prop("href");
-                        url = ui.url.setParams({
-                            themeId: color.Id
-                        });
-                        sheet.prop("href", url);
-                    }
-                    ui.theme.currentTheme = color;
-                    ui.page.fire("themeChanged", color);
+                    setHighlight(color);
                 }
             },
             function(error) {
                 ui.msgshow("修改主题失败，" + error.message, true);
             }
         );
+    },
+    /** 设置高亮色 */
+    setHighlight: function(color) {
+        if(color) {
+            setHighlight(color);
+        }
     }
 };
