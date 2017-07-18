0e950c077bafe42b1d544dc5ca884e2b71a01307
soonsoft <soonz@aliyun.com>
2017-07-18 17:05:13 +0800

    Update soon-ui.js

--- ui/soon-ui.js
+++ ui/soon-ui.js
@@ -2,7 +2,24 @@
     SoonUI 主命名空间声明
  */
 ( function( global, factory ) {
-
+	/**
+	 * 严格模式
+	 * 变量必须声明后再使用
+	 * 函数的参数不能有同名属性，否则报错
+	 * 不能使用with语句
+	 * 不能对只读属性赋值，否则报错
+	 * 不能使用前缀0表示八进制数，否则报错
+	 * 不能删除不可删除的属性，否则报错
+	 * 不能删除变量delete prop，会报错，只能删除属性delete global[prop]
+	 * eval不会在它的外层作用域引入变量
+	 * eval和arguments不能被重新赋值
+	 * arguments不会自动反映函数参数的变化
+	 * 不能使用arguments.callee
+	 * 不能使用arguments.caller
+	 * 禁止this指向全局对象
+	 * 不能使用fn.caller和fn.arguments获取函数调用的堆栈
+	 * 增加了保留字（比如protected、static和interface）
+	 */
 	"use strict";
 
 	if ( typeof module === "object" && typeof module.exports === "object" ) {
