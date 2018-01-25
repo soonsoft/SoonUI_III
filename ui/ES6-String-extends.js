f80e604648957ece5553c0840f440ae4513f5ec7
soonsoft <soonz@aliyun.com>
2018-01-25 15:35:52 +0800

    Update ES6-String-extends.js

--- ui/ES6-String-extends.js
+++ ui/ES6-String-extends.js
@@ -4,62 +4,112 @@ function isFunction(fn) {
     return ui.core.isFunction(fn);
 }
 
+function ensureInteger(position) {
+	var index = position ? Number(position) : 0;
+	if(isNaN(index)) {
+		index = 0;
+	}
+	return index;
+}
+
+var toString = Object.prototype.toString;
+
+// at
 if(!isFunction(String.prototype.at)) {
-    // https://github.com/es-shims/String.prototype.at
-    (function() {
-        'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
-        var defineProperty = (function() {
-            // IE 8 only supports `Object.defineProperty` on DOM elements.
-            try {
-                var object = {};
-                var $defineProperty = Object.defineProperty;
-                var result = $defineProperty(object, object, object) && $defineProperty;
-            } catch (exception) {}
-            return result;
-        }());
-        var at = function(position) {
-            if (this == null) {
-                throw TypeError();
-            }
-            var string = String(this);
-            var size = string.length;
-            // `ToInteger`
-            var index = position ? Number(position) : 0;
-            if (index != index) { // better `isNaN`
-                index = 0;
-            }
-            // Account for out-of-bounds indices
-            // The odd lower bound is because the ToInteger operation is
-            // going to round `n` to `0` for `-1 < n <= 0`.
-            if (index <= -1 || index >= size) {
-                return '';
-            }
-            // Second half of `ToInteger`
-            index = index | 0;
-            // Get the first code unit and code unit value
-            var cuFirst = string.charCodeAt(index);
-            var cuSecond;
-            var nextIndex = index + 1;
-            var len = 1;
-            if ( // Check if it’s the start of a surrogate pair.
-                cuFirst >= 0xD800 && cuFirst <= 0xDBFF && // high surrogate
-                size > nextIndex // there is a next code unit
-            ) {
-                cuSecond = string.charCodeAt(nextIndex);
-                if (cuSecond >= 0xDC00 && cuSecond <= 0xDFFF) { // low surrogate
-                    len = 2;
-                }
-            }
-            return string.slice(index, index + len);
-        };
-        if (defineProperty) {
-            defineProperty(String.prototype, 'at', {
-                'value': at,
-                'configurable': true,
-                'writable': true
-            });
-        } else {
-            String.prototype.at = at;
-        }
-    }());
+	String.prototype.at = function(position) {
+		var str,
+			index,
+			endIndex,
+			len,
+			firstChar, secondChar;
+
+		if(this === null || this === undefined) {
+			throw new TypeError("String.prototype.indexOf called on null or undefined");
+		}
+
+		index = ensureInteger(position);
+		index = Math.max(index, 0);
+
+		str = toString.call(this);
+		len = str.length;
+		if(index <= -1 || index >= len) {
+			return "";
+		}
+
+		first = str.charCodeAt(index);
+		endIndex = index + 1;
+		if (firstChar >= 0xD800 && firstChar <= 0xDBFF && endIndex < len) {
+			secondChar = str.charCodeAt(endIndex);
+			if(secondChar >= 0xDC00 && secondChar <= 0xDFFF) {
+				endIndex = index + 2;
+			}
+		}
+
+		return str.slice(index, endIndex);
+	};
+}
+
+// includes
+if(!isFunction(String.prototype.includes)) {
+	String.prototype.includes = function() {
+		return String.prototype.indexOf.apply(this, arguments) !== -1;
+	};
+}
+
+// startsWith
+if(!isFunction(String.prototype.startsWith)) {
+	String.prototype.startsWith = function(searchStr) {
+		var str,
+			search,
+			startIndex;
+
+		if(ui.core.isRegExp(searchStr)) {
+			throw new TypeError("Cannot call method \"startsWith\" with a regex");
+		}
+
+		if(this === null || this === undefined) {
+			throw new TypeError("String.prototype.indexOf called on null or undefined");
+		}
+
+		str = toString.call(this);
+		search = toString.call(searchStr);
+
+		if(arguments.length > 1) {
+			startIndex = ensureInteger(arguments[1]);
+		} else {
+			startIndex = 0;
+		}
+		startIndex = Math.max(startIndex, 0);
+		
+		return str.slice(startIndex, startIndex + search.length) === search;
+	};
+}
+
+// endsWith
+if(!isFunction(String.prototype.endsWith)) {
+	String.prototype.endsWith = function(searchStr) {
+		var str,
+			search,
+			endIndex;
+
+		if(ui.core.isRegExp(searchStr)) {
+			throw new TypeError("Cannot call method \"startsWith\" with a regex");
+		}
+
+		if(this === null || this === undefined) {
+			throw new TypeError("String.prototype.indexOf called on null or undefined");
+		}
+
+		str = toString.call(this);
+		search = toString.call(searchStr);
+
+		if(arguments.length > 1) {
+			endIndex = ensureInteger(arguments[1]);
+		} else {
+			endIndex = str.length;
+		}
+		endIndex = Math.min(Math.max(endIndex, 0), str.length);
+		
+		return str.slice(endIndex - search.length, endIndex) === search;
+	};
 }
