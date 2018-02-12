f524780d16916d5c2b8ea145390a9e3422264a44
soonsoft <soonz@aliyun.com>
2018-02-12 16:30:23 +0800

    Update view-model.js

--- ui/view-model.js
+++ ui/view-model.js
@@ -1,8 +1,9 @@
-// 对象观察者
+// ViewModel 模型
 
-// 观察者
 var arrayObserverPrototype = [],
-    overrideMethods = ["push", "pop", "shift", "unshift", "splice", "sort", "reverse"];
+    overrideMethods = ["push", "pop", "shift", "unshift", "splice", "sort", "reverse"],
+    hasProto = '__proto__' in {},
+    updatePrototype;
 // 劫持修改数组的API方法
 overrideMethods.forEach(function(methodName) {
     var originalMethod = arrayObserverPrototype[methodName];
@@ -10,7 +11,8 @@ overrideMethods.forEach(function(methodName) {
     arrayObserverPrototype[methodName] = function() {
         var result,
             insertedItems,
-            args = arrayObserverPrototype.slice(arguments, 0);
+            args = arrayObserverPrototype.slice(arguments, 0),
+            notice;
 
         result = originalMethod.apply(this, args);
 
@@ -24,41 +26,132 @@ overrideMethods.forEach(function(methodName) {
                 break;
         }
 
+        notice = this.__notice__;
         if(insertedItems) {
-            // insertedItems 包装ViewModel 
+            notice.arrayNotify(insertedItems);
         }
-        // notify
+        notice.dependency.notify();
         return result;
     };
 });
 
-function Observer() {
-    if(this instanceof Observer) {
-        this.initialize();
-    } else {
-        return new Observer();
+if(hasProto) {
+    updatePrototype = function(target, prototype, keys) {
+        value.__proto__ = prototype;
+    };
+} else {
+    updatePrototype = function(target, prototype, keys) {
+        var i, len, key;
+        for(i = 0, len = keys.length; i < len; i++) {
+            key = keys[i];
+            target[key] = prototype[key];
+        }
     }
 }
-Observer.prototype = {
-    constructor: Observer
+
+function defineNotifyProperty(obj, key, val, shallow) {
+    var descriptor,
+        getter,
+        setter,
+        childNotice;
+
+    descriptor = Object.getOwnPropertyDescriptor(obj, key);
+    if (descriptor && descriptor.configurable === false) {
+        return;
+    }
+
+    getter = descriptor.get;
+    setter = descriptor.set;
+
+    // 如果深度引用，则将子属性也转换为通知对象
+    if(!shallow) {
+        childNotice = new NotifyObject(val);
+    }
+
+    Object.defineProperty(obj, key, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+
+        },
+        set: function(newVal) {
+            var oldVal = getter ? getter.call(obj) : val;
+            if(oldVal === newVal || (newVal !== newVal && val !== val)) {
+                return;
+            }
+
+            if(setter) {
+                setter.call(obj, newVal);
+            } else {
+                val = newVal;
+            }
+
+            if(!shallow) {
+                // 更新通知对象
+                childNotice = new NotifyObject(newVal);
+            }
+        }
+    });
+}
+
+function createNotifyObject(obj) {
+    var isObject,
+        isArray,
+        notice;
+
+    isObject = ui.core.isObject(obj);
+    isArray = Array.isArray(obj);
+
+    if(!isObject && !isArray) {
+        return obj;
+    }
+    if(isObject && ui.core.isEmptyObject(obj)) {
+        return obj;
+    }
+
+    if(Object.hasOwnProperty("__notice__") && obj.__notice__ instanceof NotifyObject) {
+        notice = obj.__notice__;
+        // TODO notice.count++;
+    } else if((Array.isArray(obj) || ui.core.isPlainObject(obj)) && Object.isExtensible(obj)) {
+        notice = new NotifyObject(obj);
+    }
+
+    return obj;
+}
+
+function NotifyObject(value) {
+    if(this instanceof NotifyObject) {
+        this.initialize(value);
+    } else {
+        return new NotifyObject(value);
+    }
+}
+NotifyObject.prototype = {
+    constructor: NotifyObject
     initialize: function() {
-        this.__ob__ = this;
+        this.value = value;
+        this.dependency = new Dependency();
+        value.__notice__ = this;
+        if(Array.isArray(value)) {
+            updatePrototype(value, arrayObserverPrototype, overrideMethods);
+            this.arrayNotify(value);
+        } else {
+            this.objectNotify(value);
+        }
     },
+    arrayNotify: function(array) {
+        var i, len;
+        for(i = 0, len = array.length; i < len; i++) {
+            createNotifyObject(array[i]);
+        }
+    },
+    objectNotify: function(obj) {
+        var keys = Object.keys(obj),
+            i, len;
 
-};
-
-// 查看器
-function Watcher() {
-    if(this instanceof Watcher) {
-        this.initialize();
-    } else {
-        return new Watcher();
-    }
-}
-Watcher.prototype = {
-    constructor: Watcher,
-    initialize: funciton() {
-
+        for(i = 0, len = keys.length; i < len; i++) {
+            defineNotifyProperty(obj, keys[i], obj[keys[i]]);
+        }
     }
 };
 
@@ -73,13 +166,30 @@ Dependency.prototype = {
 
     },
     // 移除依赖处理
-    remove: function() {
+    remove: function(item) {
 
     },
     depend: function() {
-
     },
     notify: function() {
 
     }
 };
+
+
+// 查看器
+function Binder() {
+    if(this instanceof Binder) {
+        this.initialize();
+    } else {
+        return new Binder();
+    }
+}
+Binder.prototype = {
+    constructor: Binder,
+    initialize: funciton() {
+
+    }
+};
+
+ui.ViewModel = createNotifyObject;
