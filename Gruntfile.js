40ae63cca2efd1a0276c11f2b94b58edb1a8fc7b
soonsoft <soonz@outlook.com>
2018-03-01 21:39:30 +0800

    更新编译文件

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,6 +19,7 @@ module.exports = function(grunt) {
     ];
     let colors = require("./theme-colors");
     let images = require("./theme-inner-images");
+    let highlights = require("./theme-highlights");
 
     // layer indexes
     let layerIndexes = {
@@ -69,177 +70,6 @@ module.exports = function(grunt) {
     themeFiles.push({ "dist/theme/viewpage/viewpage.all.css": "theme/viewpage/**/*.less" });
 
     // 高亮色
-    let highlights = [
-        {
-            name: "Default",
-            description: "藏蓝",
-            "highlight-color": "#3E5A99",
-            "highlight-second-color": "#5B73A8"
-        },
-        {
-            name: "Amber",
-            description: "琥珀",
-            "highlight-color": "#F29D00",
-            "highlight-second-color": "#F4AC26"
-        },
-        {
-            name: "Blue",
-            description: "蓝色",
-            "highlight-color": "#2078EF",
-            "highlight-second-color": "#428CF1"
-        },
-        {
-            name: "BlueGray",
-            description: "蓝灰",
-            "highlight-color": "#4B8BAE",
-            "highlight-second-color": "#669DBA"
-        },
-        {
-            name: "Brown",
-            description: "褐色",
-            "highlight-color": "#752918",
-            "highlight-second-color": "#8F5144"
-        },
-        {
-            name: "Carmine",
-            description: "洋红",
-            "highlight-color": "#FF2968",
-            "highlight-second-color": "#FF497F"
-        },
-        {
-            name: "Cyan",
-            description: "青色",
-            "highlight-color": "#00CCCC",
-            "highlight-second-color": "#26D4D4"
-        },
-        {
-            name: "DarkRed",
-            description: "暗红",
-            "highlight-color": "#990000",
-            "highlight-second-color": "#AC3030"
-        },
-        {
-            name: "Golden",
-            description: "金色",
-            "highlight-color": "#BDB76B",
-            "highlight-second-color": "#C7C281"
-        },
-        {
-            name: "GrassGreen",
-            description: "草绿",
-            "highlight-color": "#99DA0D",
-            "highlight-second-color": "#ACE13B"
-        },
-        {
-            name: "Gray",
-            description: "灰色",
-            "highlight-color": "#A1A1A1",
-            "highlight-second-color": "#909090"
-        },
-        {
-            name: "Green",
-            description: "绿色",
-            "highlight-color": "#008A00",
-            "highlight-second-color": "#30A030"
-        },
-        {
-            name: "GreenLight",
-            description: "亮绿",
-            "highlight-color": "#66FF99",
-            "highlight-second-color": "#32FF77"
-        },
-        {
-            name: "Indigo",
-            description: "靛蓝",
-            "highlight-color": "#5122B5",
-            "highlight-second-color": "#724CC3"
-        },
-        {
-            name: "Jade",
-            description: "翡翠",
-            "highlight-color": "#1ABC9C",
-            "highlight-second-color": "#3DC6AB"
-        },
-        {
-            name: "Light",
-            description: "光色",
-            "highlight-color": "#FFCC00",
-            "highlight-second-color": "#FFDC52"
-        },
-        {
-            name: "Lip",
-            description: "唇色",
-            "highlight-color": "#D783A7",
-            "highlight-second-color": "#DD96B4"
-        },
-        {
-            name: "Olive",
-            description: "橄榄",
-            "highlight-color": "#8AAD92",
-            "highlight-second-color": "#9CB9A2"
-        },
-        {
-            name: "Orange",
-            description: "橙色",
-            "highlight-color": "#FF8627",
-            "highlight-second-color": "#FF9848"
-        },
-        {
-            name: "Pink",
-            description: "粉红",
-            "highlight-color": "#F567C5",
-            "highlight-second-color": "#F77ECE"
-        },
-        {
-            name: "PinkPurple",
-            description: "粉紫",
-            "highlight-color": "#A988DF",
-            "highlight-second-color": "#B69AE4"
-        },
-        {
-            name: "Purple",
-            description: "紫色",
-            "highlight-color": "#9F4AC9",
-            "highlight-second-color": "#AD65D1"
-        },
-        {
-            name: "Red",
-            description: "红色",
-            "highlight-color": "#E53935",
-            "highlight-second-color": "#E95753"
-        },
-        {
-            name: "Rose",
-            description: "玫瑰红",
-            "highlight-color": "#BF1E4B",
-            "highlight-second-color": "#C94066"
-        },
-        {
-            name: "SeaBlue",
-            description: "海蓝",
-            "highlight-color": "#0F80C1",
-            "highlight-second-color": "#3393CA"
-        },
-        {
-            name: "SkyBlue",
-            description: "天蓝",
-            "highlight-color": "#5DB2FF",
-            "highlight-second-color": "#75BEFF"
-        },
-        {
-            name: "SkyLine",
-            description: "天际蓝",
-            "highlight-color": "#00C8F8",
-            "highlight-second-color": "#52DAFA"
-        },
-        {
-            name: "SpaceGray",
-            description: "深空灰",
-            "highlight-color": "#616161",
-            "highlight-second-color": "#797979"
-        }
-    ];
-    // 高亮色
     for(let i = 0; i < highlights.length; i++) {
         let item = highlights[i];
         let key = "dist/theme/color/ui.metro." + item.name + ".css";
@@ -261,11 +91,10 @@ module.exports = function(grunt) {
         "src/ES5-Function-shims.js",
         "src/ES6-Number-shims.js",
         "src/ES5-Object-shims.js",
+        "src/ES6-promise.js",
 
-        "src/promise.js",
         "src/array-faker.js",
         "src/keyarray.js",
-        "src/introsort.js",
         
         "src/util.js",
         "src/util-string.js",
@@ -275,27 +104,26 @@ module.exports = function(grunt) {
         "src/util-structure-transform.js",
         "src/util-random.js",
 
-        "src/animation.js",
-        "src/custom-event.js",
-        "src/json.js",
-        "src/ajax.js",
-        "src/cookie.js",
-        "src/color.js",
-        "src/browser.js",
-        "src/image-loader.js",
-
         "src/jquery-extends.js",
-        "src/define.js",
-        "src/draggable.js",
-        "src/style-sheet.js",
-        "src/theme.js",
-        "src/page.js"
+        "src/cookie.js",
+        "src/style-sheet.js"
     ];
     let frameDestFile = "dist/ui-core.<%= pkg.version %>.js";
 
     // 组件文件
     let componentFiles = [
-        "src/component/**/*.js"
+        "src/component/introsort.js",
+        "src/component/animation.js",
+        "src/component/custom-event.js",
+        "src/component/json.js",
+        "src/component/ajax.js",
+        "src/component/color.js",
+        "src/component/browser.js",
+        "src/component/image-loader.js",
+        "src/component/define.js",
+        "src/component/draggable.js",
+        "src/component/theme.js",
+        "src/component/page.js"
     ];
     let componentDestFile = "dist/ui-components.<%= pkg.version %>.js";
 
@@ -353,6 +181,13 @@ module.exports = function(grunt) {
                 src: frameFiles,
                 dest: frameDestFile
             },
+            components: {
+                options: {
+                    process: option
+                },
+                src: componentFiles,
+                dest: componentDestFile
+            },
             controls: {
                 options: {
                     process: option
@@ -397,6 +232,8 @@ module.exports = function(grunt) {
                 files: {
                     // ui.core.min.js
                     "dist/ui.core.<%= pkg.version %>.min.js": ["<%= concat.frame.dest %>"],
+                    // ui.components.min.js
+                    "dist/ui.components.<%= pkg.version %>.min.js": ["<%= concat.components.dest %>"],
                     // ui.controls.min.js
                     "dist/ui.controls.<%= pkg.version %>.min.js": ["<%= concat.controls.dest %>"],
                     // ui.effect.min.js
