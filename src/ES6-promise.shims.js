dd15ea14b7e665c51df700d0657bc8f10f8b77a4
soonsoft <soonz@aliyun.com>
2018-03-13 17:10:56 +0800

    Update ES6-promise.shims.js

--- src/ES6-promise.shims.js
+++ src/ES6-promise.shims.js
@@ -1,214 +1,249 @@
-// promise
 
-//chrome36的原生Promise还多了一个defer()静态方法，允许不通过传参就能生成Promise实例，
-//另还多了一个chain(onSuccess, onFail)原型方法，意义不明
-//目前，firefox24, opera19也支持原生Promise(chrome32就支持了，但需要打开开关，自36起直接可用)
-//本模块提供的Promise完整实现ECMA262v6 的Promise规范
-function ok(val) {
-    return val;
-}
-function ng(e) {
-    throw e;
+var PromiseShim = null,
+	isFunction,
+	global;
+
+isFunction = ui.core.isFunction;
+
+function noop() {}
+
+function finally(onFinally) {
+	var P;
+	onFinally = isFunction(onFinally) ? onFinally : noop;
+
+	P = this.constructor;
+
+	return this.then(
+		function(value) {
+			P.resolve(onFinally()).then(function() {
+				return value;
+			});
+		},
+		function (reason) {
+			P.resolve(onFinally()).then(function() {
+				throw reason;
+			});
+		}
+	);
 }
 
-function done(onSuccess) {
-    //添加成功回调
-    return this.then(onSuccess, ng);
-}
-function fail(onFail) {
-    //添加出错回调
-    return this.then(ok, onFail);
-}
-function defer() {
-    var ret = {};
-    ret.promise = new this(function (resolve, reject) {
-        ret.resolve = resolve;
-        ret.reject = reject;
-    });
-    return ret;
+// 提案，暂不实现
+function try() {
+
 }
 
-var uiPromise = function (executor) {
-    this._callbacks = [];
-    var me = this;
-    if (typeof this !== "object")
-        throw new Error("Promises must be constructed via new");
-    if (typeof executor !== "function")
-        throw new Error("not a function");
-
-    executor(function (value) {
-        _resolve(me, value);
-    }, function (reason) {
-        _reject(me, reason);
-    });
-};
-//返回一个已经处于`resolved`状态的Promise对象
-uiPromise.resolve = function (value) {
-    return new uiPromise(function (resolve) {
-        resolve(value);
-    });
-};
-//返回一个已经处于`rejected`状态的Promise对象
-uiPromise.reject = function (reason) {
-    return new uiPromise(function (resolve, reject) {
-        reject(reason);
-    });
-};
-
-uiPromise.prototype = {
-    //一个Promise对象一共有3个状态：
-    //- `pending`：还处在等待状态，并没有明确最终结果
-    //- `resolved`：任务已经完成，处在成功状态
-    //- `rejected`：任务已经完成，处在失败状态
-    constructor: uiPromise,
-    _state: "pending",
-    _fired: false, //判定是否已经被触发
-    _fire: function (onSuccess, onFail) {
-        if (this._state === "rejected") {
-            if (typeof onFail === "function") {
-                onFail(this._value);
-            } else {
-                throw this._value;
-            }
-        } else {
-            if (typeof onSuccess === "function") {
-                onSuccess(this._value);
-            }
-        }
-    },
-    _then: function (onSuccess, onFail) {
-        var self;
-        if (this._fired) {//在已有Promise上添加回调
-            self = this;
-            setTimeout(function() {
-                self._fire(onSuccess, onFail);
-            }, 0);
-        } else {
-            this._callbacks.push({onSuccess: onSuccess, onFail: onFail});
-        }
-    },
-    then: function (onSuccess, onFail) {
-        onSuccess = typeof onSuccess === "function" ? onSuccess : ok;
-        onFail = typeof onFail === "function" ? onFail : ng;
-        //在新的Promise上添加回调
-        var me = this;
-        var nextPromise = new uiPromise(function (resolve, reject) {
-            me._then(function (value) {
-                try {
-                    value = onSuccess(value);
-                } catch (e) {
-                    // https://promisesaplus.com/#point-55
-                    reject(e);
-                    return;
-                }
-                resolve(value);
-            }, function (value) {
-                try {
-                    value = onFail(value);
-                } catch (e) {
-                    reject(e);
-                    return;
-                }
-                resolve(value);
-            });
-        });
-        for (var i in me) {
-            if (!personal[i]) {
-                nextPromise[i] = me[i];
-            }
-        }
-        return nextPromise;
-    },
-    "done": done,
-    "catch": fail,
-    "fail": fail
-};
-var personal = {
-    _state: 1,
-    _fired: 1,
-    _value: 1,
-    _callbacks: 1
-};
-function _resolve(promise, value) {
-    //触发成功回调
-    if (promise._state !== "pending")
-        return;
-    if (value && typeof value.then === "function") {
-        //thenable对象使用then，Promise实例使用_then
-        var method = value instanceof uiPromise ? "_then" : "then";
-        value[method](function (val) {
-            _transmit(promise, val, true);
-        }, function (reason) {
-            _transmit(promise, reason, false);
-        });
-    } else {
-        _transmit(promise, value, true);
-    }
+if(typeof Promise !== "undefined" && ui.core.isNative(Promise)) {
+	// 原生支持Promise
+	if(!isFunction(Promise.prototype.finally)) {
+		Promise.prototype.finally = finally;
+	}
+	if(!isFunction(Promise.prototype.try)) {
+		// 增加Promise.try提案的方法
+		Promise.prototype.try = try;
+	}
 }
-function _reject(promise, value) {
-    //触发失败回调
-    if (promise._state !== "pending")
-        return;
-    _transmit(promise, value, false);
-}
-//改变Promise的_fired值，并保持用户传参，触发所有回调
-function _transmit(promise, value, isResolved) {
-    promise._fired = true;
-    promise._value = value;
+
+// 生成Promise垫片
+
+// 确定Promise对象的状态，并且执行回调函数
+function transmit(promise, value, isResolved) {
+    promise._result = value;
     promise._state = isResolved ? "fulfilled" : "rejected";
-    setTimeout(function() {
+    ui.setMicroTask(function() {
         var data, i, len;
         for(i = 0, len = promise._callbacks.length; i < len; i++) {
             data = promise._callbacks[i];
             promise._fire(data.onSuccess, data.onFail);
         }
-    }, 0);
+    });
 }
-function _some(any, iterable) {
-    iterable = ui.core.type(iterable) === "array" ? iterable : [];
-    var n = 0, result = [], end;
-    return new uiPromise(function (resolve, reject) {
+
+function some(any, iterable) {
+    var n = 0, 
+    	result = [], 
+    	end,
+    	i, len;
+	
+	iterable = ui.core.type(iterable) === "array" ? iterable : [];
+    return new PromiseShim(function (resolve, reject) {
         // 空数组直接resolve
-        if (!iterable.length)
+        if (!iterable.length) {
             resolve();
-        function loop(a, index) {
-            a.then(function (ret) {
-                if (!end) {
-                    result[index] = ret;
-                    //保证回调的顺序
-                    n++;
-                    if (any || n >= iterable.length) {
-                        resolve(any ? ret : result);
-                        end = true;
-                    }
-                }
-            }, function (e) {
-                end = true;
-                reject(e);
-            });
         }
-        for (var i = 0, l = iterable.length; i < l; i++) {
+        function loop(promise, index) {
+            promise.then(
+            	function (ret) {
+	                if (!end) {
+	                    result[index] = ret;
+	                    //保证回调的顺序
+	                    n++;
+	                    if (any || n >= iterable.length) {
+	                        resolve(any ? ret : result);
+	                        end = true;
+	                    }
+	                }
+	            }, 
+	            function (e) {
+	                end = true;
+	                reject(e);
+	            }
+            );
+        }
+        for (i = 0, len = iterable.length; i < len; i++) {
             loop(iterable[i], i);
         }
     });
 }
 
-uiPromise.all = function (iterable) {
-    return _some(false, iterable);
-};
-uiPromise.race = function (iterable) {
-    return _some(true, iterable);
-};
-uiPromise.defer = defer;
-
-ui.Promise = uiPromise;
-var nativePromise = window.Promise;
-if (/native code/.test(nativePromise)) {
-    nativePromise.prototype.done = done;
-    nativePromise.prototype.fail = fail;
-    if (!nativePromise.defer) { 
-        //chrome实现的私有方法
-        nativePromise.defer = defer;
-    }
+function success(value) {
+	return value;
 }
-window.Promise = nativePromise || uiPromise;
+
+function failed(reason) {
+	throw reason;
+}
+
+PromiseShim = function(executor) {
+	var promise;
+
+	if (typeof this !== "object") {
+    	throw new TypeError("Promises must be constructed via new");
+	}
+    if (isFunction(executor)) {
+        throw new TypeError("the executor is not a function");
+    }
+
+	// Promise共有三个状态
+	// 'pending' 还处在等待状态，并没有明确最终结果
+	// 'resolved' 任务已经完成，处在成功状态
+	// 'rejected' 任务已经完成，处在失败状态
+	this._state = "pending";
+	this._callbacks = [];
+
+	promise = this;
+	executor(
+		// resolve
+		function (value) {
+			var method;
+		    if (promise._state !== "pending") {
+		        return;
+		    }
+		    if (value && isFunction(value.then)) {
+		        // thenable对象使用then，Promise实例使用_then
+		        method = value instanceof PromiseShim ? "_then" : "then";
+		        // 如果value是Promise对象则把callbacks转移到value的then当中
+		        value[method](
+		        	function (val) {
+		            	transmit(promise, val, true);
+			        }, 
+			        function (reason) {
+			            transmit(promise, reason, false);
+			        }
+		        );
+		    } else {
+		        transmit(promise, value, true);
+		    }
+	    }, 
+	    // reject
+	    function (reason) {
+		    if (promise._state !== "pending") {
+		        return;
+		    }
+		    transmit(promise, reason, false);
+	    }
+    );
+};
+PromiseShim.prototype = {
+	constructor: PromiseShim,
+	// 处理then方法的回调函数
+	_then: function(onSuccess, onFail) {
+		var that = this;
+		if (this._state !== "pending") {
+			// 如果Promise状态已经确定则异步触发回调
+			ui.setMicroTask(function() {
+	            that._fire(onSuccess, onFail);
+	        });
+	    } else {
+	        this._callbacks.push({
+	        	onSuccess: onSuccess, 
+	        	onFail: onFail
+	        });
+	    }
+	},
+	_fire: function(onSuccess, onFail) {
+		if (this._state === "rejected") {
+            if (typeof onFail === "function") {
+                onFail(this._result);
+            } else {
+                throw this._result;
+            }
+        } else {
+            if (typeof onSuccess === "function") {
+                onSuccess(this._result);
+            }
+        }
+	},
+	then: function() {
+		var that = this,
+			nextPromise;
+
+		onSuccess = isFunction(onSuccess) ? onSuccess : success;
+        onFail = isFunction(onFail) ? onFail : failed;
+
+        // 用于衔接then
+        nextPromise = new PromiseShim(function (resolve, reject) {
+            that._then(
+            	function (value) {
+	                try {
+	                    value = onSuccess(value);
+	                } catch (e) {
+	                    // https://promisesaplus.com/#point-55
+	                    reject(e);
+	                    return;
+	                }
+	                resolve(value);
+	            }, 
+	            function (value) {
+	                try {
+	                    value = onFail(value);
+	                } catch (e) {
+	                    reject(e);
+	                    return;
+	                }
+	                resolve(value);
+	            }
+            );
+        });
+
+        return nextPromise;
+	},
+	catch: function(onFail) {
+		//添加出错回调
+    	return this.then(success, onFail);
+	},
+	finally: finally,
+	try: try
+};
+
+PromiseShim.all = function(iterable) {
+	return some(false, iterable);
+};
+
+PromiseShim.race = function(iterable) {
+	return some(true, iterable);
+};
+
+PromiseShim.resolve = function(value) {
+	return new PromiseShim(function (resolve) {
+        resolve(value);
+    });
+};
+
+PromiseShim.reject = function(reason) {
+	return new PromiseShim(function (resolve, reject) {
+        reject(reason);
+    });
+};
+
+global = ui.core.global();
+global.Promise = PromiseShim;
